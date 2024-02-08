5a3c5283373ff7e182e7363faec15aa766023a30
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-02-08 23:18:38 +0900

    Add files via upload

---
+++ union/UnionType.h
@@ -0,0 +1,135 @@
+
+
+/*
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+This file was generated from UnionType.idl
+using RTI Code Generator (rtiddsgen) version 4.2.0.
+The rtiddsgen tool is part of the RTI Connext DDS distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the Code Generator User's Manual.
+*/
+
+#ifndef UnionType_240946868_h
+#define UnionType_240946868_h
+
+#ifndef NDDS_STANDALONE_TYPE
+#ifndef ndds_cpp_h
+#include "ndds/ndds_cpp.h"
+#endif
+#include "rti/xcdr/Interpreter.hpp"
+#else
+#include "ndds_standalone_type.h"
+#endif
+
+extern "C" {
+
+    extern const char *UnionTypeTYPENAME;
+
+}
+
+struct UnionTypeSeq;
+#ifndef NDDS_STANDALONE_TYPE
+class UnionTypeTypeSupport;
+class UnionTypeDataWriter;
+class UnionTypeDataReader;
+#endif
+
+typedef struct UnionType
+{
+    typedef struct UnionTypeSeq Seq;
+    #ifndef NDDS_STANDALONE_TYPE
+    typedef UnionTypeTypeSupport TypeSupport;
+    typedef UnionTypeDataWriter DataWriter;
+    typedef UnionTypeDataReader DataReader;
+    #endif
+
+    DDS_Long _d;
+    struct UnionType_u 
+    {
+
+        DDS_Long   long_data ;
+        DDS_Float   float_data ;
+        DDS_Char *   string_data ;
+    }_u;
+
+} UnionType ;
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, start exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport __declspec(dllexport)
+#endif
+
+#ifndef NDDS_STANDALONE_TYPE
+NDDSUSERDllExport DDS_TypeCode * UnionType_get_typecode(void); /* Type code */
+NDDSUSERDllExport RTIXCdrTypePlugin *UnionType_get_type_plugin_info(void);
+NDDSUSERDllExport RTIXCdrSampleAccessInfo *UnionType_get_sample_access_info(void);
+NDDSUSERDllExport RTIXCdrSampleAccessInfo *UnionType_get_sample_seq_access_info(void);
+#endif
+DDS_SEQUENCE(UnionTypeSeq, UnionType);
+
+NDDSUSERDllExport
+RTIBool UnionType_initialize(
+    UnionType* self);
+
+NDDSUSERDllExport
+RTIBool UnionType_initialize_ex(
+    UnionType* self,RTIBool allocatePointers,RTIBool allocateMemory);
+
+NDDSUSERDllExport
+RTIBool UnionType_initialize_w_params(
+    UnionType* self,
+    const struct DDS_TypeAllocationParams_t * allocParams);  
+
+NDDSUSERDllExport
+RTIBool UnionType_finalize_w_return(
+    UnionType* self);
+
+NDDSUSERDllExport
+void UnionType_finalize(
+    UnionType* self);
+
+NDDSUSERDllExport
+void UnionType_finalize_ex(
+    UnionType* self,RTIBool deletePointers);
+
+NDDSUSERDllExport
+void UnionType_finalize_w_params(
+    UnionType* self,
+    const struct DDS_TypeDeallocationParams_t * deallocParams);
+
+NDDSUSERDllExport
+void UnionType_finalize_optional_members(
+    UnionType* self, RTIBool deletePointers);  
+
+NDDSUSERDllExport
+RTIBool UnionType_copy(
+    UnionType* dst,
+    const UnionType* src);
+
+NDDSUSERDllExport
+DDS_LongLong UnionType_getDefaultDiscriminator(void);
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, stop exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport
+#endif
+
+#ifndef NDDS_STANDALONE_TYPE
+namespace rti { 
+    namespace xcdr {
+        template <>
+        struct type_code< UnionType> {
+            static const RTIXCdrTypeCode * get();
+        };
+
+    } 
+}
+
+#endif
+
+#endif /* UnionType */
+
---
+++ union/UnionTypePlugin.h
@@ -0,0 +1,205 @@
+
+
+/*
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+This file was generated from UnionType.idl
+using RTI Code Generator (rtiddsgen) version 4.2.0.
+The rtiddsgen tool is part of the RTI Connext DDS distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the Code Generator User's Manual.
+*/
+
+#ifndef UnionTypePlugin_240946868_h
+#define UnionTypePlugin_240946868_h
+
+#include "UnionType.h"
+
+struct RTICdrStream;
+
+#ifndef pres_typePlugin_h
+#include "pres/pres_typePlugin.h"
+#endif
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, start exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport __declspec(dllexport)
+#endif
+
+extern "C"{
+
+    #define UnionTypePlugin_get_sample PRESTypePluginDefaultEndpointData_getSample 
+
+    #define UnionTypePlugin_get_buffer PRESTypePluginDefaultEndpointData_getBuffer 
+    #define UnionTypePlugin_return_buffer PRESTypePluginDefaultEndpointData_returnBuffer
+
+    #define UnionTypePlugin_create_sample PRESTypePluginDefaultEndpointData_createSample 
+    #define UnionTypePlugin_destroy_sample PRESTypePluginDefaultEndpointData_deleteSample 
+
+    /* --------------------------------------------------------------------------------------
+    Support functions:
+    * -------------------------------------------------------------------------------------- */
+
+    NDDSUSERDllExport extern UnionType*
+    UnionTypePluginSupport_create_data_w_params(
+        const struct DDS_TypeAllocationParams_t * alloc_params);
+
+    NDDSUSERDllExport extern UnionType*
+    UnionTypePluginSupport_create_data_ex(RTIBool allocate_pointers);
+
+    NDDSUSERDllExport extern UnionType*
+    UnionTypePluginSupport_create_data(void);
+
+    NDDSUSERDllExport extern RTIBool 
+    UnionTypePluginSupport_copy_data(
+        UnionType *out,
+        const UnionType *in);
+
+    NDDSUSERDllExport extern void 
+    UnionTypePluginSupport_destroy_data_w_params(
+        UnionType *sample,
+        const struct DDS_TypeDeallocationParams_t * dealloc_params);
+
+    NDDSUSERDllExport extern void 
+    UnionTypePluginSupport_destroy_data_ex(
+        UnionType *sample,RTIBool deallocate_pointers);
+
+    NDDSUSERDllExport extern void 
+    UnionTypePluginSupport_destroy_data(
+        UnionType *sample);
+
+    NDDSUSERDllExport extern void 
+    UnionTypePluginSupport_print_data(
+        const UnionType *sample,
+        const char *desc,
+        unsigned int indent);
+
+    /* ----------------------------------------------------------------------------
+    Callback functions:
+    * ---------------------------------------------------------------------------- */
+
+    NDDSUSERDllExport extern PRESTypePluginParticipantData 
+    UnionTypePlugin_on_participant_attached(
+        void *registration_data, 
+        const struct PRESTypePluginParticipantInfo *participant_info,
+        RTIBool top_level_registration, 
+        void *container_plugin_context,
+        RTICdrTypeCode *typeCode);
+
+    NDDSUSERDllExport extern void 
+    UnionTypePlugin_on_participant_detached(
+        PRESTypePluginParticipantData participant_data);
+
+    NDDSUSERDllExport extern PRESTypePluginEndpointData 
+    UnionTypePlugin_on_endpoint_attached(
+        PRESTypePluginParticipantData participant_data,
+        const struct PRESTypePluginEndpointInfo *endpoint_info,
+        RTIBool top_level_registration, 
+        void *container_plugin_context);
+
+    NDDSUSERDllExport extern void 
+    UnionTypePlugin_on_endpoint_detached(
+        PRESTypePluginEndpointData endpoint_data);
+
+    NDDSUSERDllExport extern void    
+    UnionTypePlugin_return_sample(
+        PRESTypePluginEndpointData endpoint_data,
+        UnionType *sample,
+        void *handle);    
+
+    NDDSUSERDllExport extern RTIBool 
+    UnionTypePlugin_copy_sample(
+        PRESTypePluginEndpointData endpoint_data,
+        UnionType *out,
+        const UnionType *in);
+
+    /* ----------------------------------------------------------------------------
+    (De)Serialize functions:
+    * ------------------------------------------------------------------------- */
+
+    NDDSUSERDllExport extern RTIBool
+    UnionTypePlugin_serialize_to_cdr_buffer(
+        char * buffer,
+        unsigned int * length,
+        const UnionType *sample); 
+
+    NDDSUSERDllExport extern RTIBool
+    UnionTypePlugin_serialize_to_cdr_buffer_ex(
+        char *buffer,
+        unsigned int *length,
+        const UnionType *sample,
+        DDS_DataRepresentationId_t representation);
+
+    NDDSUSERDllExport extern RTIBool
+    UnionTypePlugin_deserialize_from_cdr_buffer(
+        UnionType *sample,
+        const char * buffer,
+        unsigned int length);    
+    #if !defined (NDDS_STANDALONE_TYPE)
+    NDDSUSERDllExport extern DDS_ReturnCode_t
+    UnionTypePlugin_data_to_string(
+        const UnionType *sample,
+        char *str,
+        DDS_UnsignedLong *str_size, 
+        const struct DDS_PrintFormatProperty *property);    
+    #endif
+
+    NDDSUSERDllExport extern unsigned int 
+    UnionTypePlugin_get_serialized_sample_max_size(
+        PRESTypePluginEndpointData endpoint_data,
+        RTIBool include_encapsulation,
+        RTIEncapsulationId encapsulation_id,
+        unsigned int current_alignment);
+
+    /* --------------------------------------------------------------------------------------
+    Key Management functions:
+    * -------------------------------------------------------------------------------------- */
+    NDDSUSERDllExport extern PRESTypePluginKeyKind 
+    UnionTypePlugin_get_key_kind(void);
+
+    NDDSUSERDllExport extern unsigned int 
+    UnionTypePlugin_get_serialized_key_max_size(
+        PRESTypePluginEndpointData endpoint_data,
+        RTIBool include_encapsulation,
+        RTIEncapsulationId encapsulation_id,
+        unsigned int current_alignment);
+
+    NDDSUSERDllExport extern unsigned int 
+    UnionTypePlugin_get_serialized_key_max_size_for_keyhash(
+        PRESTypePluginEndpointData endpoint_data,
+        RTIEncapsulationId encapsulation_id,
+        unsigned int current_alignment);
+
+    NDDSUSERDllExport extern RTIBool 
+    UnionTypePlugin_deserialize_key(
+        PRESTypePluginEndpointData endpoint_data,
+        UnionType ** sample,
+        RTIBool * drop_sample,
+        struct RTICdrStream *stream,
+        RTIBool deserialize_encapsulation,
+        RTIBool deserialize_key,
+        void *endpoint_plugin_qos);
+
+    NDDSUSERDllExport extern
+    struct RTIXCdrInterpreterPrograms * UnionTypePlugin_get_programs(void);
+
+    /* Plugin Functions */
+    NDDSUSERDllExport extern struct PRESTypePlugin*
+    UnionTypePlugin_new(void);
+
+    NDDSUSERDllExport extern void
+    UnionTypePlugin_delete(struct PRESTypePlugin *);
+
+}
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE) || defined(RTI_INTIME)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, stop exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport
+#endif
+
+#endif /* UnionTypePlugin_240946868_h */
+
