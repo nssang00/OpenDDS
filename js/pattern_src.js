e7672282707a26dfc9d76a0ca5dcc5b375a7669b
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-07-07 17:36:52 +0900

    Create pattern_src.js

---
+++ js/pattern_src.js
@@ -0,0 +1,65 @@
+if ('stroke-pattern-src' in style) {
+    const textureId = computeHash(style['stroke-pattern-src']);
+    const sizeExpression = parseImageProperties(
+      style,
+      builder,
+      uniforms,
+      'stroke-pattern-',
+      textureId,
+    );
+    let sampleSizeExpression = sizeExpression;
+    let offsetExpression = 'vec2(0.)';
+    if ('stroke-pattern-offset' in style && 'stroke-pattern-size' in style) {
+      sampleSizeExpression = expressionToGlsl(
+        fragContext,
+        style[`stroke-pattern-size`],
+        NumberArrayType,
+      );
+      offsetExpression = parseImageOffsetProperties(
+        style,
+        'stroke-pattern-',
+        fragContext,
+        sizeExpression,
+        sampleSizeExpression,
+      );
+    }
+    let spacingExpression = '0.';
+    if ('stroke-pattern-spacing' in style) {
+      spacingExpression = expressionToGlsl(
+        fragContext,
+        style['stroke-pattern-spacing'],
+        NumberType,
+      );
+    }
+    fragContext.functions['sampleStrokePattern'] =
+      `vec4 sampleStrokePattern(sampler2D texture, vec2 textureSize, vec2 textureOffset, vec2 sampleSize, float spacingPx, float currentLengthPx, float currentRadiusRatio, float lineWidth) {
+  float currentLengthScaled = currentLengthPx * sampleSize.y / lineWidth;
+  float spacingScaled = spacingPx * sampleSize.y / lineWidth;
+  float uCoordPx = mod(currentLengthScaled, (sampleSize.x + spacingScaled));
+  // make sure that we're not sampling too close to the borders to avoid interpolation with outside pixels
+  uCoordPx = clamp(uCoordPx, 0.5, sampleSize.x - 1.5); // Adjusted clamp range
+  float vCoordPx = (-currentRadiusRatio * 0.5 + 0.5) * sampleSize.y;
+  vec2 texCoord = (vec2(uCoordPx, vCoordPx) + textureOffset) / textureSize;
+  return samplePremultiplied(texture, texCoord);
+}`;
+    const textureName = `u_texture${textureId}`;
+    let tintExpression = '1.';
+    if ('stroke-color' in style) {
+      tintExpression = builder.getStrokeColorExpression();
+    }
+    builder.setStrokeColorExpression(
+      `${tintExpression} * sampleStrokePattern(${textureName}, ${sizeExpression}, ${offsetExpression}, ${sampleSizeExpression}, ${spacingExpression}, currentLengthPx, currentRadiusRatio, v_width)`,
+    );
+  }
+
+  if ('stroke-width' in style) {
+    builder.setStrokeWidthExpression(
+      expressionToGlsl(vertContext, style['stroke-width'], NumberType),
+    );
+  }
+
+  if ('stroke-offset' in style) {
+    builder.setStrokeOffsetExpression(
+      expressionToGlsl(vertContext, style['stroke-offset'], NumberType),
+    );
+  }
