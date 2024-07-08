2a1e336690fe94c3abcab3551b8a106f5fa8f319
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-07-08 23:10:05 +0900

    Update drawvert.js

--- js/drawvert.js
+++ js/drawvert.js
@@ -1,59 +1,4 @@
 
-function drawVerticalSubType(options) {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.scale(1, 1);
-  canvas.width = options.width+2;//5;
-
-  if(options.verticalType < 3)
-  {
-    canvas.height = Math.max(options.leftLength, options.rightLength) * 2;
-    const centerY = canvas.height / 2;
-
-    ctx.lineWidth = options.width;
-    ctx.strokeStyle = options.color;
-    ctx.lineCap = options.lineCap; // butt, square, round 
-
-  
-    ctx.beginPath();
-    if(options.leftLength > 0)
-    {
-      ctx.moveTo(canvas.width / 2, centerY);
-      ctx.lineTo(canvas.width / 2, centerY - options.leftLength);
-    }
-  
-    if(options.rightLength > 0)
-    {
-      ctx.moveTo(canvas.width / 2, centerY);
-      ctx.lineTo(canvas.width / 2, centerY + options.rightLength);
-    }
-    ctx.stroke();  
-  }
-  else
-  {
-    canvas.height = Math.max(options.leftLength, options.rightLength) * 2 + options.width;
-    const centerY = canvas.height / 2;    
-    ctx.translate(canvas.width / 2, 0);
-  
-    ctx.fillStyle = options.color;  
-  
-    if(options.leftLength > 0)
-    {
-      ctx.arc(0, centerY - options.leftLength, options.width / 2, 0, 2 * Math.PI);
-    }
-    if(options.rightLength > 0)
-    {  
-      ctx.arc(0, centerY + options.rightLength, options.width / 2, 0, 2 * Math.PI);
-    }
-    ctx.fill();
-  }
-
-  return {
-    src: canvas.toDataURL('image/png'),
-    strokeWidth: canvas.height,
-  }
-}
-
 const line_vertical2 = {
   'stroke-color': 'blue',//<Color>
   'stroke-width': 10,//<Width>
@@ -65,16 +10,15 @@ const line_vertical2 = {
   //'stroke-pattern-spacing': 80,//<Interval>    
 };
 
-const options_vertical = {
+const vertical_result = drawVerticalSubType({
   color: 'rgba(0, 0, 0, 1)',
   width: 1,
   verticalType: 0,
   leftLength: 5,
   rightLength: 5, 
   lineCap: 'butt'
-};
+});
 
-let vertical_result = drawVerticalSubType(options_vertical);
 const line_vertical = {
   'stroke-pattern-src': vertical_result.src,//<Picture>, StartCap, EndCap
   'stroke-width': vertical_result.strokeWidth,//<Width>
@@ -83,14 +27,13 @@ const line_vertical = {
   'stroke-pattern-spacing': 80,//<Interval>    
 };
 
-const options_vertica_circle1 = {
+const vertical_circle1_result = drawVerticalSubType({
   color: 'red',
   width: 3,
   verticalType: 4,
   leftLength: 5,
   rightLength: 0, 
-};
-let vertical_circle1_result = drawVerticalSubType(options_vertica_circle1);
+});
 
 const line_vertical_circle1 = {
   'stroke-pattern-src': vertical_circle1_result.src,//<Picture>
@@ -100,15 +43,14 @@ const line_vertical_circle1 = {
   'stroke-pattern-spacing': 80,//<Interval>    
 };
 
-const options_vertica_circle2 = {
+const vertical_circle2_result = drawVerticalSubType({
   color: 'green',
   width: 3,
   verticalType: 4,
   leftLength: 5,
   rightLength: 0, 
-};
+});
 
-let vertical_circle2_result = drawVerticalSubType(options_vertica_circle2);
 const line_vertical_circle2 = {
   'stroke-pattern-src': vertical_circle2_result.src,//<Picture>
   'stroke-width': vertical_circle2_result.strokeWidth,//<Width>
@@ -117,4 +59,35 @@ const line_vertical_circle2 = {
   'stroke-pattern-spacing': 80,//<Interval>    
 };
 
-const styles = [line_simple, line_vertical, line_vertical2, line_vertical_circle1, line_vertical_circle2];
+const simple_line = {
+  filter: [
+    'all',
+    ['==', ['get', 'layer'], 'landcover'],
+    ['<=', ['resolution'], 305.748113140705],
+  ],  
+  'stroke-color': 'black',
+  'stroke-width': 1,
+  'stroke-offset': 0,  
+};
+
+const simple_line_left = {
+  filter: [
+    'all',
+    ['==', ['get', 'layer'], 'landcover'],
+    ['<=', ['resolution'], 305.748113140705],
+  ],  
+  'stroke-color': 'blue',
+  'stroke-width': 1,
+  'stroke-offset': 2,  
+};
+
+const simple_right_left = {
+  filter: [
+    'all',
+    ['==', ['get', 'layer'], 'landcover'],
+    ['<=', ['resolution'], 305.748113140705],
+  ],  
+  'stroke-color': 'red',
+  'stroke-width': 1,
+  'stroke-offset':-2,  
+};
