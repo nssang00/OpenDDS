81430486007907d1dea65de099bba86498cf8bd4
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-07-10 07:12:48 +0900

    Update glsl.js

--- js/glsl.js
+++ js/glsl.js
@@ -17,3 +17,19 @@ vec4 sampleStrokePattern(sampler2D texture, vec2 textureSize, vec2 textureOffset
   
   return samplePremultiplied(texture, texCoord);
 }
+
+
+vec4 sampleStrokePattern(sampler2D texture, vec2 textureSize, vec2 textureOffset, vec2 sampleSize, float spacingPx, float startOffsetPx, float currentLengthPx, float currentRadiusRatio, float lineWidth) {
+  float currentLengthScaled = currentLengthPx * sampleSize.y / lineWidth;
+  float spacingScaled = spacingPx * sampleSize.y / lineWidth;
+
+  float patternRepeat = sampleSize.x + spacingScaled;
+  float adjustedLength = mod(currentLengthScaled + startOffsetPx, patternRepeat);
+  
+  float uCoordPx = clamp(adjustedLength, 0.5, sampleSize.x - 0.5);
+  
+  float vCoordPx = (-currentRadiusRatio * 0.5 + 0.5) * sampleSize.y;
+  vec2 texCoord = (vec2(uCoordPx, vCoordPx) + textureOffset) / textureSize;
+  
+  return samplePremultiplied(texture, texCoord);
+}
