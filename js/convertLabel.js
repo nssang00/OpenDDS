7d1ddd943891e2160f9182a68ea812cc8271a797
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-07-20 01:19:54 +0900

    Update convertLabel.js

--- js/convertLabel.js
+++ js/convertLabel.js
@@ -31,3 +31,44 @@ let res = convertLabel({
   underline: true,
   subscript: true,
 });
+/////////
+
+function createConverter(options) {
+  const { prefix, postfix, decimal, underline, subscript } = options;
+
+  return function(text) {
+    let num = parseFloat(text);
+    if (!isNaN(num)) {
+      if (decimal >= 0) {
+        text = num.toFixed(decimal);
+      }
+      if (subscript) {
+        const [integerPart, decimalPart] = text.split('.');
+        const subscriptNumbers = ['₀', '₁', '₂', '₃', '₄', '₅', '₆', '₇', '₈', '₉'];
+        text = integerPart;
+        const subscriptDecimal = decimalPart?.split('').map(char => subscriptNumbers[parseInt(char)]).join('');
+        if (subscriptDecimal) {
+          text = text + '.' + subscriptDecimal;
+        }
+      }
+    }
+
+    let result = prefix + text + postfix;
+    if (underline) {
+      result = result.split('').map((char) => char + '\u0332').join('');
+    }
+    return result;
+  };
+}
+
+let evaluator = createConverter({
+  prefix: '',
+  postfix: '',
+  decimal: -1,
+  underline: true,
+  subscript: true,
+});
+
+// 사용 예시
+let result = evaluator('123.35');
+console.log(result);
