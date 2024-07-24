97a626240a8a9b01efa19119ec9ca3cba621b228
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-07-24 09:27:30 +0900

    Update await.js

--- js/await.js
+++ js/await.js
@@ -1,36 +1,36 @@
 function createStrokePatternStyle(src) {
   let pattern;
-  (async () => {
 
-    const loadImage = (src) => {
-      return new Promise((resolve, reject) => {
-          const img = new Image();
-          img.src = src;
-          img.onload = () => {
-            const canvas = document.createElement('canvas');
-            canvas.width = Math.round(img.width);  
-            canvas.height = Math.round(img.height);
-            const ctx = canvas.getContext('2d');
-      
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-            pattern = ctx.createPattern(canvas, 'repeat');
-            console.log('resolve ' + canvas.width)
-            resolve(pattern);
-          };
-          img.onerror = reject;
-      });
-    };
+  (async () => {
     try {
-      await loadImage(src);
+      const img = new Image();
+      img.src = src;
+
+      // 이미지를 로드하는 동안 대기
+      pattern = await new Promise((resolve, reject) => {
+        img.onload = () => {
+          const canvas = document.createElement('canvas');
+          canvas.width = Math.round(img.width);  
+          canvas.height = Math.round(img.height);
+          const ctx = canvas.getContext('2d');
+
+          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+          const createdPattern = ctx.createPattern(canvas, 'repeat');
+          console.log('resolve ' + canvas.width);
+          resolve(createdPattern);
+        };
+
+        img.onerror = () => {
+          reject(new Error('이미지를 로드하는 동안 오류가 발생했습니다.'));
+        };
+      });
+
     } catch (error) {
-        console.error('이미지를 로드하는 동안 오류가 발생했습니다:', error);
+      console.error(error);
     }
-    console.log('wait ' + pattern)
+    console.log('wait ' + pattern);
   })();
-console.log('ret ' + pattern)  
+
+  console.log('ret ' + pattern);
   return pattern;
-  //flatStyle['stroke-color'] = pattern;
-  //delete flatStyle['stroke-pattern-src'];
-  //console.log('ret')
-  //return flatStyle;
 }
