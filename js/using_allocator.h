b701e5caaac0d8e1b6dbcbd8fabdc63081134b07
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-08-23 21:13:53 +0900

    Update using_allocator.h

--- js/using_allocator.h
+++ js/using_allocator.h
@@ -1,14 +1,3 @@
-#ifdef USE_CUSTOM_ALLOCATOR
-    // USE_STC_MM가 정의되어 있으면 CustomAllocator 사용
-    template <typename T>
-    typedef CustomAllocator<T> STC_ALLOCATOR;
-#else
-    // USE_STC_MM가 정의되어 있지 않으면 std::allocator 사용
-    template <typename T>
-    typedef std::allocator<T> STC_ALLOCATOR;
-#endif
-
-std::vector<int, STC_ALLOCATOR<int>> vec;
 
 #ifdef USE_CUSTOM_ALLOCATOR
     template<typename T>
@@ -25,10 +14,6 @@ std::vector<int, STC_ALLOCATOR<int>> vec;
 // 사용 예시: USE_CUSTOM_ALLOCATOR가 정의된 경우 CustomAllocator 사용, 그렇지 않은 경우 std::allocator 사용
 std::vector<int, STC_ALLOCATOR<int>::type> myVector;
 
-
-
-// Allocator 선택을 위한 매크로 정의 (필요시)
-#define USE_CUSTOM_ALLOCATOR
 
 // allocator를 선택하는 typedef
 #ifdef USE_CUSTOM_ALLOCATOR
