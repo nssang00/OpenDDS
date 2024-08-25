3d6eea7423598243ded147ad7b088f827b8a5270
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-08-25 17:16:21 +0900

    Update bind_lambda.h

--- js/bind_lambda.h
+++ js/bind_lambda.h
@@ -9,7 +9,28 @@ class SpeedTest {
 public:
 };
 
-std::bind(&SpeedTest::SumDataBlock, this, 3, 5);
-[this](){
-    SumDataBlock(3, 5);
+template <typename Func, typename... Args>
+class bind_t {
+    Func func;          // 원본 함수
+    std::tuple<Args...> args; // 전달받은 인자들을 저장
+
+public:
+    bind_t(Func f, Args... a) : func(f), args(a...) {}
+
+    template <typename... CallArgs>
+    auto operator()(CallArgs&&... callArgs) {
+        return call_with_args(std::index_sequence_for<Args...>{}, std::forward<CallArgs>(callArgs)...);
+    }
+
+private:
+    template <size_t... Is, typename... CallArgs>
+    auto call_with_args(std::index_sequence<Is...>, CallArgs&&... callArgs) {
+        return func(resolve_arg<Is>(std::forward<CallArgs>(callArgs)...)...);
+    }
+
+    template <size_t I, typename... CallArgs>
+    auto resolve_arg(CallArgs&&... callArgs) {
+        // args에 저장된 각 인자를 평가. 자리표시자는 전달된 인자를 사용.
+        return std::get<I>(args)(std::forward<CallArgs>(callArgs)...);
+    }
 };
