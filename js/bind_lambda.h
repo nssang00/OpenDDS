359c9a741b319f487e3c4a69c2e80a9f0dd084bb
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-08-25 18:41:00 +0900

    Update bind_lambda.h

--- js/bind_lambda.h
+++ js/bind_lambda.h
@@ -34,3 +34,68 @@ private:
         return std::get<I>(args)(std::forward<CallArgs>(callArgs)...);
     }
 };
+
+
+/////?
+#include <functional>
+#include <tuple>
+#include <type_traits>
+
+namespace my_std {
+
+// 함수 호출을 위한 헬퍼 함수
+template<typename Func, typename Tuple, std::size_t... I>
+auto call_impl(Func&& f, Tuple&& t, std::index_sequence<I...>) {
+    return std::forward<Func>(f)(std::get<I>(std::forward<Tuple>(t))...);
+}
+
+template<typename Func, typename Tuple>
+auto call(Func&& f, Tuple&& t) {
+    constexpr auto size = std::tuple_size<std::decay_t<Tuple>>::value;
+    return call_impl(std::forward<Func>(f), std::forward<Tuple>(t),
+                     std::make_index_sequence<size>{});
+}
+
+// placeholder 타입
+template<int>
+struct placeholder {};
+
+// 실제 바인드 구현
+template<typename F, typename... Args>
+class binder {
+    F f;
+    std::tuple<Args...> args;
+
+public:
+    binder(F&& f, Args&&... args)
+        : f(std::forward<F>(f)), args(std::forward<Args>(args)...) {}
+
+    template<typename... CallArgs>
+    auto operator()(CallArgs&&... call_args) {
+        auto bound_args = std::tuple_cat(
+            args,
+            std::forward_as_tuple(std::forward<CallArgs>(call_args)...)
+        );
+        return call(f, bound_args);
+    }
+};
+
+// bind 함수
+template<typename F, typename... Args>
+auto bind(F&& f, Args&&... args) {
+    return binder<std::decay_t<F>, std::decay_t<Args>...>(
+        std::forward<F>(f), std::forward<Args>(args)...
+    );
+}
+
+} // namespace my_std
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    auto bound = my_std::bind(add, 5, my_std::placeholder<1>{});
+    std::cout << bound(10) << std::endl; // 출력: 15
+    return 0;
+}
