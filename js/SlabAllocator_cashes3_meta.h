8d5dc9e91e07851a3c27f2c21b00c359e43bf6ab
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-08-27 18:07:50 +0900

    Update SlabAllocator_cashes3_meta.h

--- js/SlabAllocator_cashes3_meta.h
+++ js/SlabAllocator_cashes3_meta.h
@@ -90,3 +90,62 @@ void SlabAllocator::free(void* ptr) {
         if (it != cache->slabMap.end() && (unsigned char*)ptr < (unsigned char*)it->first + it->second->blockSize * it->second->capacity) {
             // 슬랩의 범위 내에 속한 ptr을 찾음
             slabDeallocate(it->second, ptr);
+            break;
+        }
+    }
+    LeaveCriticalSection(&cs);
+}
+
+SlabAllocator::SlabCache* SlabAllocator::getCache(size_t size) {
+    size_t index = size;
+    if (index >= caches.size()) {
+        caches.resize(index + 1, NULL);
+    }
+    if (!caches[index]) {
+        caches[index] = new SlabCache(size);
+    }
+    return caches[index];
+}
+
+SlabAllocator::Slab* SlabAllocator::createSlab(size_t size) {
+    size_t capacity = 1024;
+    size_t alignedSize = (size + 63) & ~63; // 캐시 라인 크기 맞춤
+    return new Slab(alignedSize, capacity);
+}
+
+void* SlabAllocator::slabAllocate(Slab* slab) {
+    if (slab->freeCount > 0) {
+        size_t index = slab->freeList.back();
+        slab->freeList.pop_back();
+        --slab->freeCount;
+        return slab->data + index * slab->blockSize;
+    }
+    return NULL;
+}
+
+void SlabAllocator::slabDeallocate(Slab* slab, void* ptr) {
+    size_t index = ((unsigned char*)ptr - slab->data) / slab->blockSize;
+    if (index < slab->capacity) {
+        slab->freeList.push_back(index);
+        ++slab->freeCount;
+    }
+}
+
+SlabAllocator::SlabCache::~SlabCache() {
+    for (size_t i = 0; i < slabs.size(); ++i) {
+        delete slabs[i];
+    }
+}
+
+SlabAllocator::Slab::Slab(size_t blkSize, size_t cap)
+    : blockSize(blkSize), capacity(cap), freeCount(cap) {
+    data = new unsigned char[blkSize * cap];
+    freeList.reserve(cap); // 필요한 용량만큼 미리 할당
+    for (size_t i = 0; i < cap; ++i) {
+        freeList.push_back(i);
+    }
+}
+
+SlabAllocator::Slab::~Slab() {
+    delete[] data;
+}
