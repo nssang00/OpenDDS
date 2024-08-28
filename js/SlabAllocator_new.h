a39b095b8ea52f07aa3fd50f10948f0c097c33c0
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-08-28 10:37:05 +0900

    Update SlabAllocator_new.h

--- js/SlabAllocator_new.h
+++ js/SlabAllocator_new.h
@@ -31,6 +31,7 @@ private:
     size_t getObjectSize(void* ptr);
 };
 
+// Singleton instance
 SlabAllocator* SlabAllocator::Instance() {
     static SlabAllocator instance;
     return &instance;
@@ -39,11 +40,10 @@ SlabAllocator* SlabAllocator::Instance() {
 SlabAllocator::SlabAllocator() {}
 
 SlabAllocator::~SlabAllocator() {
-    std::map<size_t, SlabCache*>::iterator it;
-    for (it = slabCaches.begin(); it != slabCaches.end(); ++it) {
+    for (std::map<size_t, SlabCache*>::iterator it = slabCaches.begin(); it != slabCaches.end(); ++it) {
         SlabCache* cache = it->second;
-        for (size_t i = 0; i < cache->slabs.size(); ++i) {
-            ::free(cache->slabs[i]);
+        for (std::vector<void*>::iterator vecIt = cache->slabs.begin(); vecIt != cache->slabs.end(); ++vecIt) {
+            ::free(*vecIt);
         }
         delete cache;
     }
@@ -52,24 +52,25 @@ SlabAllocator::~SlabAllocator() {
 void SlabAllocator::addSlab(size_t size) {
     SlabCache* cache;
     
-    // 기존 캐시가 없으면 새로 생성
-    if (slabCaches.find(size) == slabCaches.end()) {
+    // Get or create cache
+    std::map<size_t, SlabCache*>::iterator it = slabCaches.find(size);
+    if (it == slabCaches.end()) {
         cache = new SlabCache();
         cache->objectSize = size;
         cache->freeList = NULL;
         slabCaches[size] = cache;
     } else {
-        cache = slabCaches[size];
+        cache = it->second;
     }
 
-    // 메모리 할당 (객체 크기 + 크기 정보용 메타데이터)
+    // Allocate a new slab
     void* newSlab = ::malloc((size + sizeof(size_t)) * 8);
     cache->slabs.push_back(newSlab);
 
     char* slabPtr = static_cast<char*>(newSlab);
 
     for (int i = 0; i < 8; ++i) {
-        // 객체 크기 기록
+        // Store object size at the start of the block
         *reinterpret_cast<size_t*>(slabPtr) = size;
         Slab* slab = reinterpret_cast<Slab*>(slabPtr + sizeof(size_t));
         slab->next = cache->freeList;
@@ -79,10 +80,9 @@ void SlabAllocator::addSlab(size_t size) {
 }
 
 void* SlabAllocator::allocate(size_t size) {
-void* SlabAllocator::allocate(size_t size) {
     SlabCache* cache;
 
-    // 해당 크기의 캐시가 없다면 생성
+    // Get or create cache
     std::map<size_t, SlabCache*>::iterator it = slabCaches.find(size);
     if (it == slabCaches.end()) {
         addSlab(size);
@@ -97,16 +97,16 @@ void* SlabAllocator::allocate(size_t size) {
     Slab* slab = cache->freeList;
     cache->freeList = slab->next;
 
-    // 실제 객체 시작 주소를 반환
+    // Return pointer to the allocated object
     return reinterpret_cast<void*>(reinterpret_cast<char*>(slab) + sizeof(size_t));
 }
 
 void SlabAllocator::free(void* ptr) {
-    // 저장된 크기 정보를 이용해 슬랩 캐시를 찾음
+    // Get object size to find the correct cache
     size_t size = getObjectSize(ptr);
     SlabCache* cache = slabCaches[size];
 
-    // 메모리를 해제하고 자유 리스트로 반환
+    // Adjust pointer to find the start of the slab
     char* actualPtr = reinterpret_cast<char*>(ptr) - sizeof(size_t);
     Slab* slab = reinterpret_cast<Slab*>(actualPtr);
     slab->next = cache->freeList;
@@ -114,7 +114,7 @@ void SlabAllocator::free(void* ptr) {
 }
 
 size_t SlabAllocator::getObjectSize(void* ptr) {
-    // ptr에서 sizeof(size_t)만큼 뒤로 이동하여 메모리 블록의 시작 부분을 찾습니다.
+    // Adjust pointer to the start of the slab
     char* actualPtr = reinterpret_cast<char*>(ptr) - sizeof(size_t);
     return *reinterpret_cast<size_t*>(actualPtr);
 }
