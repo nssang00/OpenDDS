cdd8e7b0ed306d42305f88b6bacc7d24705c012b
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-09-03 07:45:34 +0900

    Update SlabAllocator6.h

--- js/SlabAllocator6.h
+++ js/SlabAllocator6.h
@@ -2,22 +2,25 @@
 #include <vector>
 #include <windows.h>  // For CRITICAL_SECTION
 
-// Slab Allocator 클래스 정의
-class SlabAllocator {
+class CustomAllocator {
 public:
-    // 생성자
-    SlabAllocator(size_t objectSize, size_t slabSize);
-    
+    // Singleton 인스턴스 접근 함수
+    static CustomAllocator* Instance(int num = 0);
+
     // 메모리 할당 및 해제 함수
-    void* allocate();
-    void deallocate(void* object);
+    void* allocate(size_t size);
+    void free(void* object, size_t size);
 
     // 소멸자
-    ~SlabAllocator();
+    ~CustomAllocator();
 
 private:
+    // Private 생성자
+    CustomAllocator(size_t objectSize = 64, size_t slabSize = 100);
+
+    // Slab 구조체 정의
     struct Slab {
-        void* start;          // Slab 시작 주소
+        void* start;                // Slab 시작 주소
         std::vector<void*> freeList;  // 사용되지 않은 블록 리스트
     };
 
@@ -25,21 +28,26 @@ private:
     size_t slabSize;    // Slab의 크기
     std::vector<Slab> slabs;  // Slab 리스트
 
-    CRITICAL_SECTION criticalSection;  // 멀티스레드 환경에서의 동기화를 위한 CRITICAL_SECTION
+    CRITICAL_SECTION criticalSection;  // 동기화용 CRITICAL_SECTION
 
-    // 새로운 Slab을 생성하는 함수
+    static CustomAllocator* instance[2];  // Singleton 인스턴스 배열
+
+    // 새로운 Slab 생성 함수
     void createNewSlab();
 };
 
-// 생성자 구현
-SlabAllocator::SlabAllocator(size_t objectSize, size_t slabSize) 
+// Singleton 인스턴스 정의
+CustomAllocator* CustomAllocator::instance[2] = { NULL, NULL };
+
+// Private 생성자 구현
+CustomAllocator::CustomAllocator(size_t objectSize, size_t slabSize) 
     : objectSize(objectSize), slabSize(slabSize) {
     InitializeCriticalSection(&criticalSection);  // CRITICAL_SECTION 초기화
     createNewSlab();  // 초기 Slab 생성
 }
 
 // Slab 생성 함수 구현
-void SlabAllocator::createNewSlab() {
+void CustomAllocator::createNewSlab() {
     EnterCriticalSection(&criticalSection);  // CRITICAL_SECTION 잠금
 
     Slab newSlab;
@@ -56,9 +64,22 @@ void SlabAllocator::createNewSlab() {
     LeaveCriticalSection(&criticalSection);  // CRITICAL_SECTION 잠금 해제
 }
 
+// Singleton 인스턴스 접근 함수
+CustomAllocator* CustomAllocator::Instance(int num) {
+    if (!instance[num]) {
+        instance[num] = new CustomAllocator();  // 새 인스턴스 생성
+    }
+    return instance[num];  // 인스턴스 반환
+}
+
 // 메모리 할당 함수 구현
-void* SlabAllocator::allocate() {
+void* CustomAllocator::allocate(size_t size) {
     EnterCriticalSection(&criticalSection);  // CRITICAL_SECTION 잠금
+
+    if (size > objectSize) {
+        LeaveCriticalSection(&criticalSection);
+        return NULL;  // 요청 크기가 슬랩 크기보다 큰 경우 NULL 반환
+    }
 
     for (std::vector<Slab>::iterator it = slabs.begin(); it != slabs.end(); ++it) {
         Slab& slab = *it;
@@ -73,12 +94,18 @@ void* SlabAllocator::allocate() {
     // 모든 Slab이 가득 찼다면 새로운 Slab 생성
     createNewSlab();
     LeaveCriticalSection(&criticalSection);  // CRITICAL_SECTION 잠금 해제
-    return allocate();  // 새로운 Slab에서 다시 할당 시도
+    return allocate(size);  // 새로운 Slab에서 다시 할당 시도
 }
 
 // 메모리 해제 함수 구현
-void SlabAllocator::deallocate(void* object) {
+void CustomAllocator::free(void* object, size_t size) {
     EnterCriticalSection(&criticalSection);  // CRITICAL_SECTION 잠금
+
+    if (size > objectSize) {
+        LeaveCriticalSection(&criticalSection);
+        std::cerr << "Error: Deallocating invalid object!" << std::endl;
+        return;
+    }
 
     for (std::vector<Slab>::iterator it = slabs.begin(); it != slabs.end(); ++it) {
         Slab& slab = *it;
@@ -91,12 +118,11 @@ void SlabAllocator::deallocate(void* object) {
 
     // 이 객체는 이 Slab Allocator에서 할당된 것이 아님
     std::cerr << "Error: Deallocating invalid object!" << std::endl;
-
     LeaveCriticalSection(&criticalSection);  // CRITICAL_SECTION 잠금 해제
 }
 
 // 소멸자 구현
-SlabAllocator::~SlabAllocator() {
+CustomAllocator::~CustomAllocator() {
     EnterCriticalSection(&criticalSection);  // CRITICAL_SECTION 잠금
 
     for (std::vector<Slab>::iterator it = slabs.begin(); it != slabs.end(); ++it) {
@@ -111,13 +137,13 @@ SlabAllocator::~SlabAllocator() {
 
 // 테스트 함수
 int main() {
-    SlabAllocator allocator(64, 100);  // 객체 크기 64바이트, Slab 크기 100개의 객체
+    CustomAllocator* allocator = CustomAllocator::Instance();  // Singleton 인스턴스 가져오기
 
-    void* obj1 = allocator.allocate();  // 객체 할당
-    void* obj2 = allocator.allocate();  // 또 다른 객체 할당
+    void* obj1 = allocator->allocate(64);  // 객체 할당
+    void* obj2 = allocator->allocate(64);  // 또 다른 객체 할당
 
-    allocator.deallocate(obj1);  // 첫 번째 객체 해제
-    allocator.deallocate(obj2);  // 두 번째 객체 해제
+    allocator->free(obj1, 64);  // 첫 번째 객체 해제
+    allocator->free(obj2, 64);  // 두 번째 객체 해제
 
     return 0;
 }
