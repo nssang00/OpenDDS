878cd00f9f37350d2fe6d88e0500f913c182f3f4
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-09-11 07:51:08 +0900

    Update background2.js

--- extension/background2.js
+++ extension/background2.js
@@ -1,9 +1,11 @@
 let detectedStreams = []; // 감지된 m3u8 URL 저장
 let playerTabId = null; // 새 탭의 ID를 저장
+let streamDataPerTab = {};
 
 // m3u8 스트림 감지 및 헤더 수정 규칙 동적 추가
 chrome.webRequest.onHeadersReceived.addListener(
   function (details) {
+     const tabId = details.tabId;
     let contentType = '';
 
     // Content-Type 헤더를 찾아서 확인
@@ -13,15 +15,17 @@ chrome.webRequest.onHeadersReceived.addListener(
         break;
       }
     }
-
-    // m3u8 스트림의 Content-Type 확인
+/////////
     if (contentType.includes('application/vnd.apple.mpegurl') || contentType.includes('application/x-mpegurl')) {
-      // URL이 중복되지 않으면 추가
-      if (!detectedStreams.includes(details.url)) {
-        detectedStreams.push(details.url);
-        // 팝업 버튼의 숫자 업데이트
-        chrome.action.setBadgeText({ text: detectedStreams.length.toString() });
+      if (!streamDataPerTab[tabId]) {
+        streamDataPerTab[tabId] = [];
       }
+      if (!streamDataPerTab[tabId].includes(details.url)) {
+        streamDataPerTab[tabId].push(details.url);
+      }
+
+      // 해당 탭의 스트림 개수를 배지에 표시
+      chrome.action.setBadgeText({ text: streamDataPerTab[tabId].length.toString(), tabId: tabId });
 
       // 동적으로 헤더 수정 규칙 추가
       chrome.declarativeNetRequest.updateSessionRules({
@@ -83,3 +87,40 @@ chrome.webRequest.onCompleted.addListener(
   },
   { urls: ["<all_urls>"] }
 );
+
+// 탭이 전환되었을 때 실행되는 함수
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  const tabId = activeInfo.tabId;
+
+  // 활성화된 탭의 스트림 데이터를 가져와 배지 업데이트
+  chrome.tabs.get(tabId, (tab) => {
+    if (tab.url && streamDataPerTab[tabId]) {
+      // 배지에 스트림 개수 표시
+      chrome.action.setBadgeText({ text: streamDataPerTab[tabId].length.toString(), tabId: tabId });
+    } else {
+      // 감지된 스트림이 없으면 배지를 비웁니다.
+      chrome.action.setBadgeText({ text: "", tabId: tabId });
+    }
+  });
+});
+
+// 탭의 URL이 변경되거나 페이지가 완전히 로드되었을 때 실행되는 함수
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    // 페이지가 완전히 로드되었을 때 처리
+    if (streamDataPerTab[tabId]) {
+      // 감지된 스트림이 있으면 배지 업데이트
+      chrome.action.setBadgeText({ text: streamDataPerTab[tabId].length.toString(), tabId: tabId });
+    } else {
+      // 감지된 스트림이 없으면 배지를 비움
+      chrome.action.setBadgeText({ text: "", tabId: tabId });
+    }
+  }
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'getStreamCount') {
+    const tabId = message.tabId;
+    const streamCount = streamDataPerTab[tabId] ? streamDataPerTab[tabId].length : 0;
+    sendResponse({ count: streamCount });
+  }
+});  
