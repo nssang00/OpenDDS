40935683f82d2554f241d1338eac55a7d8efa04f
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-09-10 19:09:45 +0900

    Update background.js

--- extension/sample/background.js
+++ extension/sample/background.js
@@ -20,6 +20,33 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   } 
 });
 
+/*
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      chrome.tabs.sendMessage(tab.id, {
+          type: 'notify',
+          payload: {
+              message: '안녕, 이것은 Background에서 chrome.tabs.sendMessage으로 보내는 메시지야~',
+          },
+      }, () => {});
+  });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0];
+        chrome.tabs.sendMessage(
+            tab.id, {
+                type: 'greeting',
+                payload: {
+                    message: '안녕~ 이것은 popup에서 보내는 메시지야~',
+                },
+            },
+            (response) => {
+                console.log(response);
+            }
+        );
+    });
+
+*/
+
 // 팝업이 열릴 때 데이터를 저장
 chrome.action.onClicked.addListener((tab) => {
   chrome.storage.local.set({ data: dataToSend });
