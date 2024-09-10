76443f077b4eb3b9cf944aed652fe18e41bbda0a
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-09-10 19:02:32 +0900

    Update background.js

--- extension/sample/background.js
+++ extension/sample/background.js
@@ -5,13 +5,19 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("Extension installed!");
 });
 
-// 메시지 리스너
+//this will listen to the message from the content script and send it to 
+//the content scripts of all the active tabs
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "getData") {
-    sendResponse({ data: dataToSend });
-  } else if (request.message === "sendData") {
-    console.log("Data received from content script:", request.data);
-  }
+  if (request.type === "FROM_TAB") {
+      chrome.tabs.query({}, (tabs) => {
+        tabs.forEach(tab => {
+          if (tab.id && tab.id !== sender.tab?.id) {
+            chrome.tabs.sendMessage(tab.id, { type: 'TO_TAB', data  });
+          }
+        });
+      });    
+    //sendResponse({ data: dataToSend });
+  } 
 });
 
 // 팝업이 열릴 때 데이터를 저장
