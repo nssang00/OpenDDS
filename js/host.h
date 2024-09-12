dc19f0a53baa6b3e5a12d3a3632819a96607d8d7
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-09-12 13:00:56 +0900

    Update host.h

--- js/host.h
+++ js/host.h
@@ -21,3 +21,31 @@ const char *GetHostAddress(int n)
 	}
 	return hostAddr[n];
 }
+
+const char *GetHostAddress(int n)
+{
+	if (HasAssignedHostInterface() == true)
+		return GetHostInterface();
+
+	static NetworkInterfaceList netIfList; // static으로 선언
+	std::string strAddress;
+
+	// 네트워크 인터페이스 목록이 비어 있으면 목록을 초기화
+	if (netIfList.getCount() == 0) {
+		GetHostAddresses(netIfList);
+	}
+
+	// n번째 인터페이스가 유효한지 확인 후 주소 반환
+	if (n < netIfList.getCount()) {
+		NetworkInterface *netif = netIfList.getNetworkInterface(n);
+		strAddress = netif->getAddress();
+	} else {
+		strAddress = "";
+	}
+
+	// 함수 내에서 주소를 담을 임시 변수를 선언
+	static char addrBuffer[40];
+	strcpy(addrBuffer, strAddress.c_str());
+
+	return addrBuffer;
+}
