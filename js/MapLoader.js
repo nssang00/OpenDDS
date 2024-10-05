51c5f8f29b0e05d7de572a7702f5b23470aa2932
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-10-06 00:35:05 +0900

    Update MapLoader.js

--- js/MapLoader.js
+++ js/MapLoader.js
@@ -193,27 +193,6 @@ const mapLoader = new MapLoader();
   }
 })();
 
-//////////////////////
-///////////////////////
-// MapStyler 인터페이스 정의
-class MapStyler {
-    applyMap(map, styleData, layerData) {
-        throw new Error("applyMap 메서드는 구현되어야 합니다.");
-    }
-}
-/*
-class MapStyler {
-    applyMap(map) {
-      throw new Error("applyMap method must be implemented");
-    }
-    buildMapLayer(layersObj) {
-      throw new Error("buildMapLayer method must be implemented");
-    }
-    buildMapStyle(compiledStyles) {
-      throw new Error("buildMapStyle method must be implemented");
-    }
-}
-*/
 // OlMapStyler 클래스가 MapStyler를 구현
 class OlMapStyler extends MapStyler {
     applyMap(map, styleData, layerData) {
@@ -239,45 +218,6 @@ class OlMapStyler extends MapStyler {
     }
 }
 
-// 맵을 빌드하고 적용하는 클래스
-class MapBuilder {
-    constructor(styler) {
-        this.styler = styler;  // 스타일링 및 레이어 처리 객체 주입
-    }
-
-    async loadMap({ styleUrl, layerUrl }) {
-        // 각각의 XML 데이터를 URL로부터 비동기적으로 로드하고 JSON으로 변환
-        const styleXmlData = await this.fetchXML(styleUrl);
-        const layerXmlData = await this.fetchXML(layerUrl);
-
-        // 스타일과 레이어 데이터를 하나의 객체로 묶어서 반환
-        const styleJsonData = this.parseXML(styleXmlData);
-        const layerJsonData = this.parseXML(layerXmlData);
-
-        return { styleData: styleJsonData, layerData: layerJsonData };
-    }
-
-    async applyMap(map, { styleUrl, layerUrl }) {
-        // 1. XML 데이터를 URL로부터 비동기적으로 로드하고 하나의 객체로 묶기
-        const mapData = await this.loadMap({ styleUrl, layerUrl });
-
-        // 2. 스타일 및 레이어 적용
-        this.styler.applyMap(map, mapData.styleData, mapData.layerData); // 객체에서 각각의 데이터 추출하여 넘김
-    }
-
-    async fetchXML(url) {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.text();  // XML 문자열로 반환
-    }
-
-    parseXML(xmlData) {
-        // XML을 JSON으로 변환하는 로직
-        return jsonToXML(xmlData);  // XML을 파싱하여 JSON 형식으로 반환
-    }
-}
 
 import MapLayerBuilder from './mapLayerBuilder.js';
 import OlMapStyler from './olMapStyler.js';
@@ -288,7 +228,4 @@ mapBuilder.applyMap(map, {
     styleUrl: 'https://example.com/path/to/style.xml',
     layerUrl: 'https://example.com/path/to/layer.xml'
 });
-
-
-
 
