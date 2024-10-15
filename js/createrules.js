2becfe6e61563a89b7cdcd6d0954053fed09fbfe
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-10-15 14:17:23 +0900

    Update createrules.js

--- js/createrules.js
+++ js/createrules.js
@@ -31,45 +31,13 @@ function createRulesToOlStyles(rules) {
         return null;
     }
 
-    let compiledRules = 
-        
-    let symbolStyles = []; // symbol 관련 스타일 규칙
-    let flatStyles = [];   // 일반 style 규칙
-
-    for (const rule of rules) {
-        if (rule.symbol) {
-            // symbol이 존재하는 경우
-            const processedSymbol = processSymbol(rule.symbol); // ol flatStyle 형태로 변경 시도
-            if (processedSymbol) {// filter를 적용하여 processedSymbol을 처리
-                flatStyles.push(...processedSymbol.map(style => ({ filter: rule.filter, ...style })));
-            } else {// filter 기반의 스타일 함수로 변환하여 저장
-                symbolStyles.push(rule);
-            }
-        } else if (rule.style) {
-            flatStyles.push({ filter: rule.filter, ...rule.style });
-        }
-    }
-
-    return [
-        ...(symbolStyles.length ? [rulesToStyleFunction(symbolStyles)] : []),
-        ...(flatStyles.length ? [flatStyles] : [])
-    ];
-    
-}
-
-function createRulesToOlStyles(rules) {
-    if (!rules) {
-        return null;
-    }
-
     const compiledRules = {
         canvas: [],  // canvas 관련 규칙 저장
         webgl: []    // webgl 관련 규칙 저장
     };
 
     for (const rule of rules) {
-        if (rule.symbol) {
-            // symbol이 존재하는 경우
+        if (rule.symbol) {// symbol이 존재하는 경우
             const processedSymbol = processSymbol(rule.symbol); // ol flatStyle 형태로 변경 시도
             if (processedSymbol) {// filter를 적용하여 processedSymbol을 처리
                 compiledRules.webgl.push(...processedSymbol.map(style => ({ filter: rule.filter, ...style })));
