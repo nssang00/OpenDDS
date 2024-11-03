6992e4292e1d6eb179cc0040d8bcd5ad8e1a8a0f
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-11-04 06:34:43 +0900

    Update producer_consumer.h

--- customallocator/producer_consumer.h
+++ customallocator/producer_consumer.h
@@ -5,42 +5,17 @@
 std::queue<int> dataQueue;
 bool done = false;
 
-// Cond 클래스: CRITICAL_SECTION과 CONDITION_VARIABLE을 감싸는 클래스
-class Cond {
+// Mutex 클래스: CRITICAL_SECTION을 감싸는 클래스
+class Mutex {
 public:
-    Cond() {
+    Mutex() {
         InitializeCriticalSection(&cs);
-        InitializeConditionVariable(&condVar);
     }
 
-    ~Cond() {
+    ~Mutex() {
         DeleteCriticalSection(&cs);
     }
 
-    // 잠금과 함께 조건 변수를 사용하여 대기
-    void wait() {
-        EnterCriticalSection(&cs);
-        while (dataQueue.empty() && !done) {
-            SleepConditionVariableCS(&condVar, &cs, INFINITE);
-        }
-        LeaveCriticalSection(&cs);
-    }
-
-    // 단일 스레드를 깨움
-    void notify() {
-        EnterCriticalSection(&cs);
-        WakeConditionVariable(&condVar);
-        LeaveCriticalSection(&cs);
-    }
-
-    // 모든 대기 중인 스레드를 깨움
-    void notifyAll() {
-        EnterCriticalSection(&cs);
-        WakeAllConditionVariable(&condVar);
-        LeaveCriticalSection(&cs);
-    }
-
-    // 데이터 접근에 대한 잠금 제어
     void lock() {
         EnterCriticalSection(&cs);
     }
@@ -51,46 +26,71 @@ public:
 
 private:
     CRITICAL_SECTION cs;
+    friend class Cond;
+};
+
+// Cond 클래스: CONDITION_VARIABLE을 감싸는 클래스
+class Cond {
+public:
+    Cond() {
+        InitializeConditionVariable(&condVar);
+    }
+
+    void wait(Mutex& mutex) {
+        SleepConditionVariableCS(&condVar, &mutex.cs, INFINITE);
+    }
+
+    void notify() {
+        WakeConditionVariable(&condVar);
+    }
+
+private:
     CONDITION_VARIABLE condVar;
 };
 
-// 전역 Cond 객체
+// 전역 Cond 및 Mutex 객체
 Cond cond;
+Mutex mutex;
 
 // 생산자 함수 (Producer)
 DWORD WINAPI producer(LPVOID lpParam) {
     for (int i = 1; i <= 10; ++i) {
-        cond.lock();
+        mutex.lock();
         dataQueue.push(i);
         std::cout << "Produced: " << i << std::endl;
-        cond.unlock();
-        cond.notify();  // 소비자에게 알림
+        cond.notify();  // 개별 소비자에게 알림
+        mutex.unlock();
     }
 
-    // 모든 데이터 생산 완료
-    cond.lock();
+    // 모든 데이터 생산 완료 표시
+    mutex.lock();
     done = true;
-    cond.unlock();
-    cond.notifyAll();  // 모든 대기 스레드 알림
+    cond.notify();  // 마지막으로 대기 중인 소비자에게 알림
+    mutex.unlock();
     return 0;
 }
 
 // 소비자 함수 (Consumer)
 DWORD WINAPI consumer(LPVOID lpParam) {
     while (true) {
-        cond.wait();  // 조건 변수 대기
+        mutex.lock();
+        
+        // 조건 변수를 이용해 데이터가 있거나 done 플래그가 true가 될 때까지 대기
+        while (dataQueue.empty() && !done) {
+            cond.wait(mutex);
+        }
 
-        // 큐에 쌓인 모든 데이터를 한 번에 가져오기
-        cond.lock();
+        // 큐에서 데이터를 가져와 처리
         while (!dataQueue.empty()) {
             int value = dataQueue.front();
             dataQueue.pop();
             std::cout << "Consumed: " << value << std::endl;
         }
-        bool finished = done && dataQueue.empty();
-        cond.unlock();
-
+        
         // 종료 조건 확인
+        bool finished = done && dataQueue.empty();
+        mutex.unlock();
+
         if (finished) {
             break;
         }
