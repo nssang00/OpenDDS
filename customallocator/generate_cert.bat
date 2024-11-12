272a88ab1ab43e6cf9cb72fda72473ec87872984
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-11-12 19:18:22 +0900

    Update generate_cert.bat

--- customallocator/generate_cert.bat
+++ customallocator/generate_cert.bat
@@ -1,43 +1,36 @@
 @echo off
-:: OpenSSL 경로 설정 (필요한 경우)
-set OPENSSL_PATH=C:\OpenSSL-Win64\bin
-set PATH=%OPENSSL_PATH%;%PATH%
+:: 배치 파일에서 OpenSSL 경로 설정 (필요한 경우)
+set PATH="C:\Program Files\Git\usr\bin\";%PATH%
 
-:: Permissions CA에 사용할 비밀번호
+:: 설정할 비밀번호
 set PASSWORD=core_1234
 
-:: 1. Root CA 생성 제거
-
-:: 2. Identity CA 생성
+:: 1. Identity CA 생성
 echo Creating Identity CA...
 openssl genrsa -out identity_ca_private_key.pem 2048
-openssl req -new -key identity_ca_private_key.pem -out identity_ca_cert.csr -subj "/CN=IdentityCA"
-openssl x509 -req -in identity_ca_cert.csr -CA identity_ca_cert.pem -CAkey identity_ca_private_key.pem -CAcreateserial -out identity_ca_cert.pem -days 3650 -sha256
-echo Identity CA created: identity_ca_private_key.pem, identity_ca_cert.pem
+::openssl req -config identity_ca_openssl.cnf -new -key identity_ca_private_key.pem -out identity_ca.csr
+openssl req -new -key identity_ca_private_key.pem -out identity_ca.csr -subj "/C=KR/O=Hanwha Systems Inc./CN=Hanwha Systems (Identity CA)"
+openssl x509 -req -days 3650 -in identity_ca.csr -signkey identity_ca_private_key.pem -out identity_ca_cert.pem
 
-:: 3. Permissions CA 생성 (암호 설정)
+:: 2. Identity Certificate 생성
 echo Creating Permissions CA...
-openssl genrsa -aes256 -passout pass:%PASSWORD% -out permissions_ca_private_key.pem 2048
-openssl req -new -key permissions_ca_private_key.pem -passin pass:%PASSWORD% -out permissions_ca_cert.csr -subj "/CN=PermissionsCA"
-openssl x509 -req -in permissions_ca_cert.csr -CA identity_ca_cert.pem -CAkey identity_ca_private_key.pem -CAcreateserial -out permissions_ca_cert.pem -days 3650 -sha256
-echo Permissions CA created: permissions_ca_private_key.pem, permissions_ca_cert.pem
+openssl genrsa -passout pass:%PASSWORD% -out permissions_ca_private_key.pem 2048
+::openssl req -config permissions_ca_openssl.cnf -new -key permissions_ca_private_key.pem -out permissions_ca.csr
+openssl req -new -key permissions_ca_private_key.pem -passin pass:%PASSWORD% -out permissions_ca.csr -subj "/C=KR/O=Hanwha Systems Inc./CN=Hanwha Systems (Permissions CA)"
+::openssl x509 -req -days 3650 -in  permissions_ca.csr -signkey permissions_ca_private_key.pem -out permissions_ca_cert.pem
+openssl x509 -req -in permissions_ca.csr -CA identity_ca_cert.pem -CAkey identity_ca_private_key.pem -CAcreateserial -out permissions_ca_cert.pem -days 3650 -sha256
 
-:: 4. governance.xml 및 permissions.xml에 서명 생성
-echo Signing governance.xml and permissions.xml with permissions CA...
-openssl smime -sign -in governance.xml -text -out signed_governance.p7s -signer permissions_ca_cert.pem -inkey permissions_ca_private_key.pem -passin pass:%PASSWORD%
-openssl smime -sign -in permissions.xml -text -out signed_permissions.p7s -signer permissions_ca_cert.pem -inkey permissions_ca_private_key.pem -passin pass:%PASSWORD%
-echo Signed files created: signed_governance.p7s, signed_permissions.p7s
+
+echo Creating Identity Certificate...
+openssl genrsa -passout pass:%PASSWORD% -out smartdds_identity_private_key.pem 2048
+::openssl req -config identity_ca_openssl.cnf -new -key smartdds_identity_private_key.pem -out smartdds_identity_ca.csr
+openssl req -new -key smartdds_identity_private_key.pem -passin pass:%PASSWORD% -out smartdds_identity.csr -subj "/C=KR/O=Hanwha Systems Inc./CN=Hanwha Systems (Identity CA)"
+::openssl x509 -req -days 3650 -in smartdds_identity.csr -signkey smartdds_identity_private_key.pem -out smartdds_identity_cert.pem
+openssl x509 -req -in smartdds_identity.csr -CA identity_ca_cert.pem -CAkey identity_ca_private_key.pem -CAcreateserial -out smartdds_identity_cert.pem -days 3650 -sha256
+
 
 :: 종료 메시지
 echo All certificates, private keys, and signed XML files have been created.
-
-:: 5. 인증서 검증
-echo Verifying certificates...
-
-:: Identity CA로 Identity CA 인증서 검증
-openssl verify -CAfile identity_ca_cert.pem identity_ca_cert.pem
-
-:: Identity CA로 Permissions CA 인증서 검증
-openssl verify -CAfile identity_ca_cert.pem permissions_ca_cert.pem
-
 pause
+
+"C:\Program Files\Git\usr\bin\openssl.exe" verify -CAfile identity_ca_cert.pem smartdds_identity_cert.pem
