641aa4a417922863db429d9dca3b2e570cc15426
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-11-19 17:08:10 +0900

    Update MapLoader.js

--- js/MapLoader.js
+++ js/MapLoader.js
@@ -423,7 +423,20 @@ const layersObj = [
     ]
   }
 ];
+/////////////////
+import { MapStyler } from './mapLayerBuilder.js';
 
+// OlMapStyler 정의
+class OlMapStyler extends MapStyler {
+  applyMap(map, { styles, layers }) {
+    console.log("Applying OpenLayers styles and layers", styles, layers);
+    // OpenLayers 스타일과 레이어를 맵에 적용하는 로직 구현
+  }
+}
+
+export default OlMapStyler;
+
+///////////
 
 
 import MapLayerBuilder from './mapLayerBuilder.js';
