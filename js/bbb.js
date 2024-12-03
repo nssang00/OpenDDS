53cfb9a0a9d3a474bbae9642b3d0a2d6c0f18c22
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-12-03 09:44:59 +0900

    Create bbb.js

---
+++ js/bbb.js
@@ -0,0 +1,38 @@
+function buildStyledOlLayers(styleObj, layersObj, urlTemplate) {
+    return layersObj.map(layerObj => {
+        let styledLayers = [];
+
+        if (layerObj.layers) {
+            styledLayers = buildStyledOlLayers(styleObj, layerObj.layers, urlTemplate);
+        } else {
+            const layerSource = getOrCreateLayerSource(layerObj.source, urlTemplate);
+
+            for (const rule of layerObj.rules) {
+                const filteredStyles = [];
+                for (const styleName of rule.styleNames) {
+                    if (!styleObj[styleName]) {
+                        throw new Error(`Style '${styleName}' not found in styleObj`);
+                    }
+
+                    const styles = [].concat(styleObj[styleName]);
+                    for (const style of styles) {
+                        filteredStyles.push({ ...style, filter: rule.filter });
+                    }
+                }
+
+                styledLayers.push(
+                    createStyledLayers({
+                        name: rule.name,
+                        styles: processRulesToOlStyles(filteredStyles),
+                        source: layerSource,
+                    })
+                );
+            }
+        }
+
+        return new LayerGroup({
+            name: layerObj.name,
+            layers: styledLayers,
+        });
+    });
+}
