faffb12518315f7fc3efbc4550d12fb3fd7d2dca
nssang00 <34025004+nssang00@users.noreply.github.com>
2024-12-13 12:47:49 +0900

    Update innerdata.js

--- js/innerdata.js
+++ js/innerdata.js
@@ -1,5 +1,48 @@
 import VectorTile from 'ol/VectorTile';
 
+class MyTile {
+  constructor() {
+    this.features = ['feature1', 'feature2', 'feature3']; // MyTile 클래스 내부 변수
+  }
+
+  upload() {
+    // VectorTile 객체 생성
+    const tile = new VectorTile();
+
+    // tile.customMethod가 없으면 추가
+    if (!tile.customMethod) {
+      tile.customMethod = (() => {
+        let internalData = null;
+
+        // 화살표 함수로 정의하여 `this`가 MyTile 인스턴스를 참조
+        return (action, data) => {
+          if (action === 'set') {
+            internalData = data;
+          } else if (action === 'get') {
+            if (!internalData) {
+              internalData = [];
+            }
+            console.log('Accessing MyTile features:', this.features);  // this.features에 접근
+            return internalData;
+          }
+        };
+      })();
+    }
+
+    // customMethod 호출
+    tile.customMethod('get');  // this.features에 접근하려면 'get' 호출
+  }
+}
+
+// MyTile 객체 생성
+const myTileInstance = new MyTile();
+
+// upload 함수 호출
+myTileInstance.upload();
+
+////////////
+import VectorTile from 'ol/VectorTile';
+
 // VectorTile 객체 생성
 const tile = new VectorTile();
 
