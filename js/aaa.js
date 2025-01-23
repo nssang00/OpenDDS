0f03e515f1011643a34bea4a85de5b8494831573
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-01-23 15:52:36 +0900

    Update aaa.js

--- js/aaa.js
+++ js/aaa.js
@@ -4,3 +4,47 @@ buildLineStyle(lineStyleObj) {
     }
     return lineStyleObj.symbolizers.map(symbolizer => this.buildLineSymbolizer(symbolizer));
 }
+
+
+buildPointStyle(pointStyleObj) {
+    if (!pointStyleObj.symbolizers) {
+        return { style: {} }; // Return an empty style object if symbolizers is not present
+    }
+
+    const offset = [Number(pointStyleObj.OffsetX), Number(pointStyleObj.OffsetY)];
+    for (const symbolizer of pointStyleObj.symbolizers) {
+        switch (symbolizer.type) {
+            case "SIMPLE":
+                if (symbolizer.Shape === "0") { // Circle
+                    return {
+                        style: {
+                            'circle-radius': symbolizer.Size,
+                            'circle-fill-color': this.toRGBAArray(symbolizer.Color),
+                            'circle-displacement': offset,
+                        }
+                    };
+                } else if (symbolizer.Shape === "1") { // Rectangle
+                    return {
+                        style: {
+                            'shape-points': 4,
+                            'shape-radius': symbolizer.Size,
+                            'shape-fill-color': this.toRGBAArray(symbolizer.Color),
+                            'shape-displacement': offset,
+                            'shape-angle': Math.PI / 4,
+                        }
+                    };
+                }
+                break;
+            case "PICTURE":
+                return {
+                    style: {
+                        'icon-src': `${this.baseSymbolPath}${symbolizer.Picture}`,
+                        'icon-displacement': offset,
+                    }
+                };
+                break;    
+            default:
+                break;
+        }
+    }
+}
