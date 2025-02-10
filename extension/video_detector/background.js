47b37fd19d0881deeee27e12a1aeca03f62e13bc
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-02-10 09:27:26 +0900

    Update background.js

--- extension/video_detector/background.js
+++ extension/video_detector/background.js
@@ -1,11 +1,8 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'downloadVideo') {
-    const filename = request.title.replace(/[^a-z0-9]/gi, '_') + '.mp4';
-    
-    chrome.downloads.download({
-      url: request.url,
-      filename: filename,
-      conflictAction: 'uniquify'
-    });
-  }
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "download_video") {
+        chrome.downloads.download({
+            url: message.url,
+            filename: "video.mp4"
+        });
+    }
 });
