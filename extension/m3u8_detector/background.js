3abf0e202ac7c9579986f42c48ecdf111216a6a6
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-02-11 18:03:22 +0900

    Update background.js

--- extension/m3u8_detector/background.js
+++ extension/m3u8_detector/background.js
@@ -5,10 +5,10 @@ chrome.webRequest.onHeadersReceived.addListener(
         );
 
         if (contentTypeHeader && /application\/(vnd\.apple\.mpegurl|x-mpegURL)/i.test(contentTypeHeader.value)) {
-            console.log("Detected M3U8 stream:", details.url, "on tab", details.tabId);
+            console.log("Detected M3U8:", details.url, "on tab", details.tabId);
 
             if (details.tabId > 0) {
-                // 해당 요청이 발생한 탭에만 메시지 전송
+                // 감지된 M3U8을 content script로 즉시 전달
                 chrome.tabs.sendMessage(details.tabId, { action: "newM3U8", url: details.url });
             }
         }
