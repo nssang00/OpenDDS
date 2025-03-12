d826bc605f7b31a64bae4dbce76323bd9a0e6277
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-03-12 21:36:20 +0900

    Update renderinstructions.js

--- js/10.4.0/renderinstructions.js
+++ js/10.4.0/renderinstructions.js
@@ -4,6 +4,9 @@
 import {transform2D} from '../../geom/flat/transform.js';
 import {apply as applyTransform} from '../../transform.js';
 
+//kmg
+import {inflateEnds} from '../../geom/flat/orient.js';
+
 /**
  * @param {Float32Array} renderInstructions Render instructions
  * @param {import('./VectorStyleRenderer.js').AttributeDefinitions} customAttributes Custom attributes
@@ -113,22 +116,21 @@ export function generateLineStringRenderInstructions(
   renderInstructions,
   customAttributes,
   transform,
+  features,//kmg
 ) {
   //kmg
-  let features = [];
+  let features2 = [];
   for (const featureUid in batch.entries) {
     const batchEntry = batch.entries[featureUid];
-    features.push(batchEntry.feature);
+    features2.push(batchEntry.feature);
   }
   
   let verticesCount = 0;
   let geometriesCount = 0;
-  for (const feature of features) {
+  for (const feature of features2) {
     const geometry = feature.getGeometry();
     verticesCount += geometry.getFlatCoordinates().length / geometry.getStride();
     geometriesCount += geometry.getEnds().length;
-
-    console.log('linestring',feature.getGeometry().getType())
   }
   
   const totalInstructionsCount2 =
@@ -138,7 +140,7 @@ export function generateLineStringRenderInstructions(
   let renderIndex2 = 0;
   let renderInstructions2 = new Float32Array(totalInstructionsCount2);
   
-  for (const feature of features) {
+  for (const feature of features2) {
     const geometry = feature.getGeometry();
     const flatCoordinates = geometry.getFlatCoordinates();
     const stride = geometry.getStride();
@@ -248,23 +250,120 @@ export function generatePolygonRenderInstructions(
   renderInstructions,
   customAttributes,
   transform,
+  features,//kmg
 ) {
   //kmg
-  let features = [];
+  let features2 = [];
   for (const featureUid in batch.entries) {
     const batchEntry = batch.entries[featureUid];
-    features.push(batchEntry.feature);
+    features2.push(batchEntry.feature);
   }
   
   let verticesCount = 0;
   let geometriesCount = 0;
-  for (const feature of features) {
+  let ringsCount = 0;
+  
+  for (const feature of features2) {
     const geometry = feature.getGeometry();
+    const flatCoordinates = geometry.getFlatCoordinates();
+    const ends = geometry.getEnds();
+    const multiPolygonEndss = inflateEnds(flatCoordinates, ends);
+    if (multiPolygonEndss.length > 1) {
+      for (let i = 0, ii = multiPolygonEndss.length; i < ii; i++) {
+        let polygonEnds = multiPolygonEndss[i];
+        const prevPolygonEnds = i > 0 ? multiPolygonEndss[i - 1] : null;
+        const startIndex = prevPolygonEnds
+          ? prevPolygonEnds[prevPolygonEnds.length - 1]
+          : 0;
+        const endIndex = polygonEnds[polygonEnds.length - 1];
+        polygonEnds =
+          startIndex > 0
+            ? polygonEnds.map((end) => end - startIndex)
+            : polygonEnds;
+            /*
+        this.addCoordinates_(
+          'Polygon',
+          flatCoords.slice(startIndex, endIndex),
+          polygonEnds,
+          feature,
+          featureUid,
+          stride,
+          layout,
+        );*/
+      }
+    }
+
+
+
+
+
     verticesCount += geometry.getFlatCoordinates().length / geometry.getStride();
-    geometriesCount += geometry.getEnds().length;
+    geometriesCount++;
+    ringsCount += ends.length;
+  }
+  console.log('polygon', features, features2);
 
 
+/////////////
 
+  const totalInstructionsCount2 =
+    2 * verticesCount +
+    (1 + getCustomAttributesSize(customAttributes)) * geometriesCount +
+    ringsCount;
+
+  const flatCoords2 = [];
+  let renderIndex2 = 0;
+  let renderInstructions2 = new Float32Array(totalInstructionsCount2);
+
+  for (const feature of features2) {
+    const geometry = feature.getGeometry();
+    const flatCoordinates = geometry.getFlatCoordinates();
+    const stride = geometry.getStride();
+    const ends = geometry.getEnds();
+
+    flatCoords2.length = flatCoordinates.length;
+    transform2D(
+      flatCoordinates,
+      0,
+      flatCoordinates.length,//flatCoords2.length,
+      stride,
+      transform,
+      flatCoords2,
+      stride,
+    );
+
+    for (const key in customAttributes) {
+      const attr = customAttributes[key];
+      const value = attr.callback.call({ref:1}, feature);
+      const size = attr.size ?? 1;
+
+      for (let j = 0; j < size; j++) {
+        renderInstructions2[renderIndex2++] = value[j] ?? value;
+      }
+    }
+
+    const ringsVerticesCount = ends.map((end, ind, arr) =>
+      ind > 0 ? (end - arr[ind - 1]) / stride : end / stride,
+    );
+    // ring count
+    renderInstructions2[renderIndex2++] = ringsVerticesCount.length;
+
+    // vertices count in each ring
+    for (let j = 0, jj = ringsVerticesCount.length; j < jj; j++) {
+      renderInstructions2[renderIndex2++] = ringsVerticesCount[j];
+    }
+
+    let offset = 0;    
+    for(let i = 0, ii = ends.length; i < ii; i++) {
+      let end = ends[i];
+
+      // looping on points for positions
+      for (let j = offset; j < end; j += stride) {
+        renderInstructions2[renderIndex2++] = flatCoords2[j]; 
+        renderInstructions2[renderIndex2++] = flatCoords2[j + 1];
+      }
+      offset = end;
+    }
   }
 
   // here we anticipate the amount of render instructions for polygons:
@@ -326,5 +425,6 @@ export function generatePolygonRenderInstructions(
       }
     }
   }
-  return renderInstructions;
+  console.log('renderinstruction', features, totalInstructionsCount, totalInstructionsCount2, renderIndex, renderIndex2)
+  return renderInstructions2;
 }
