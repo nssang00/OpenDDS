e62f0773c509b4a68dcb3d2d08bb9c895d9a2c8e
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-03-21 10:26:40 +0900

    Create stroke_shader.js

---
+++ js/10.4.0/stroke_shader.js
@@ -0,0 +1,297 @@
+//vertex
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+uniform mat4 u_projectionMatrix;
+uniform mat4 u_screenToWorldMatrix;
+uniform vec2 u_viewportSizePx;
+uniform float u_pixelRatio;
+uniform float u_globalAlpha;
+uniform float u_time;
+uniform float u_zoom;
+uniform float u_resolution;
+uniform float u_rotation;
+uniform vec4 u_renderExtent;
+uniform vec2 u_patternOrigin;
+uniform float u_depth;
+uniform mediump int u_hitDetection;
+
+const float PI = 3.141592653589793238;
+const float TWO_PI = 2.0 * PI;
+float currentLineMetric = 0.; // an actual value will be used in the stroke shaders
+
+uniform sampler2D u_depthMask;
+uniform float u_tileZoomLevel;
+attribute vec2 a_segmentStart;
+attribute vec2 a_segmentEnd;
+attribute float a_measureStart;
+attribute float a_measureEnd;
+attribute float a_parameters;
+attribute float a_distance;
+attribute vec2 a_joinAngles;
+attribute vec4 a_hitColor;
+attribute float a_prop_class;
+attribute float a_prop_layer;
+varying vec2 v_segmentStart;
+varying vec2 v_segmentEnd;
+varying float v_angleStart;
+varying float v_angleEnd;
+varying float v_width;
+varying vec4 v_hitColor;
+varying float v_distanceOffsetPx;
+varying float v_measureStart;
+varying float v_measureEnd;
+varying float v_prop_class;
+varying float v_prop_layer;
+
+vec2 worldToPx(vec2 worldPos) {
+  vec4 screenPos = u_projectionMatrix * vec4(worldPos, 0.0, 1.0);
+  return (0.5 * screenPos.xy + 0.5) * u_viewportSizePx;
+}
+
+vec4 pxToScreen(vec2 pxPos) {
+  vec2 screenPos = 2.0 * pxPos / u_viewportSizePx - 1.0;
+  return vec4(screenPos, u_depth, 1.0);
+}
+
+bool isCap(float joinAngle) {
+  return joinAngle < -0.1;
+}
+
+vec2 getJoinOffsetDirection(vec2 normalPx, float joinAngle) {
+  float halfAngle = joinAngle / 2.0;
+  float c = cos(halfAngle);
+  float s = sin(halfAngle);
+  vec2 angleBisectorNormal = vec2(s * normalPx.x + c * normalPx.y, -c * normalPx.x + s * normalPx.y);
+  float length = 1.0 / s;
+  return angleBisectorNormal * length;
+}
+
+vec2 getOffsetPoint(vec2 point, vec2 normal, float joinAngle, float offsetPx) {
+  // if on a cap or the join angle is too high, offset the line along the segment normal
+  if (cos(joinAngle) > 0.998 || isCap(joinAngle)) {
+    return point - normal * offsetPx;
+  }
+  // offset is applied along the inverted normal (positive offset goes "right" relative to line direction)
+  return point - getJoinOffsetDirection(normal, joinAngle) * offsetPx;
+}
+
+void main(void) {
+  v_angleStart = a_joinAngles.x;
+  v_angleEnd = a_joinAngles.y;
+  float vertexNumber = floor(abs(a_parameters) / 10000. + 0.5);
+  currentLineMetric = vertexNumber < 1.5 ? a_measureStart : a_measureEnd;
+  // we're reading the fractional part while keeping the sign (so -4.12 gives -0.12, 3.45 gives 0.45)
+  float angleTangentSum = fract(abs(a_parameters) / 10000.) * 10000. * sign(a_parameters);
+
+  float lineWidth = 2.0;
+  float lineOffsetPx = 0.;
+
+  // compute segment start/end in px with offset
+  vec2 segmentStartPx = worldToPx(a_segmentStart);
+  vec2 segmentEndPx = worldToPx(a_segmentEnd);
+  vec2 tangentPx = normalize(segmentEndPx - segmentStartPx);
+  vec2 normalPx = vec2(-tangentPx.y, tangentPx.x);
+  segmentStartPx = getOffsetPoint(segmentStartPx, normalPx, v_angleStart, lineOffsetPx),
+  segmentEndPx = getOffsetPoint(segmentEndPx, normalPx, v_angleEnd, lineOffsetPx);
+  
+  // compute current vertex position
+  float normalDir = vertexNumber < 0.5 || (vertexNumber > 1.5 && vertexNumber < 2.5) ? 1.0 : -1.0;
+  float tangentDir = vertexNumber < 1.5 ? 1.0 : -1.0;
+  float angle = vertexNumber < 1.5 ? v_angleStart : v_angleEnd;
+  vec2 joinDirection;
+  vec2 positionPx = vertexNumber < 1.5 ? segmentStartPx : segmentEndPx;
+  // if angle is too high, do not make a proper join
+  if (cos(angle) > 0.985 || isCap(angle)) {
+    joinDirection = normalPx * normalDir - tangentPx * tangentDir;
+  } else {
+    joinDirection = getJoinOffsetDirection(normalPx * normalDir, angle);
+  }
+  positionPx = positionPx + joinDirection * (lineWidth * 0.5 + 1.); // adding 1 pixel for antialiasing
+  gl_Position = pxToScreen(positionPx);
+
+  v_segmentStart = segmentStartPx;
+  v_segmentEnd = segmentEndPx;
+  v_width = lineWidth;
+  v_hitColor = a_hitColor;
+  v_distanceOffsetPx = a_distance / u_resolution - (lineOffsetPx * angleTangentSum);
+  v_measureStart = a_measureStart;
+  v_measureEnd = a_measureEnd;
+  v_prop_class = a_prop_class;
+  v_prop_layer = a_prop_layer;
+} 
+//fragment
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+uniform mat4 u_projectionMatrix;
+uniform mat4 u_screenToWorldMatrix;
+uniform vec2 u_viewportSizePx;
+uniform float u_pixelRatio;
+uniform float u_globalAlpha;
+uniform float u_time;
+uniform float u_zoom;
+uniform float u_resolution;
+uniform float u_rotation;
+uniform vec4 u_renderExtent;
+uniform vec2 u_patternOrigin;
+uniform float u_depth;
+uniform mediump int u_hitDetection;
+
+const float PI = 3.141592653589793238;
+const float TWO_PI = 2.0 * PI;
+float currentLineMetric = 0.; // an actual value will be used in the stroke shaders
+
+uniform sampler2D u_depthMask;
+uniform float u_tileZoomLevel;
+varying vec2 v_segmentStart;
+varying vec2 v_segmentEnd;
+varying float v_angleStart;
+varying float v_angleEnd;
+varying float v_width;
+varying vec4 v_hitColor;
+varying float v_distanceOffsetPx;
+varying float v_measureStart;
+varying float v_measureEnd;
+varying float v_prop_class;
+varying float v_prop_layer;
+bool operator_in_0(float inputValue) {
+  if (inputValue == 1.0) { return true; }
+  if (inputValue == 2.0) { return true; }
+  return false;
+}
+
+vec2 pxToWorld(vec2 pxPos) {
+  vec2 screenPos = 2.0 * pxPos / u_viewportSizePx - 1.0;
+  return (u_screenToWorldMatrix * vec4(screenPos, 0.0, 1.0)).xy;
+}
+
+bool isCap(float joinAngle) {
+  return joinAngle < -0.1;
+}
+
+float segmentDistanceField(vec2 point, vec2 start, vec2 end, float width) {
+  vec2 tangent = normalize(end - start);
+  vec2 normal = vec2(-tangent.y, tangent.x);
+  vec2 startToPoint = point - start;
+  return abs(dot(startToPoint, normal)) - width * 0.5;
+}
+
+float buttCapDistanceField(vec2 point, vec2 start, vec2 end) {
+  vec2 startToPoint = point - start;
+  vec2 tangent = normalize(end - start);
+  return dot(startToPoint, -tangent);
+}
+
+float squareCapDistanceField(vec2 point, vec2 start, vec2 end, float width) {
+  return buttCapDistanceField(point, start, end) - width * 0.5;
+}
+
+float roundCapDistanceField(vec2 point, vec2 start, vec2 end, float width) {
+  float onSegment = max(0., 1000. * dot(point - start, end - start)); // this is very high when inside the segment
+  return length(point - start) - width * 0.5 - onSegment;
+}
+
+float roundJoinDistanceField(vec2 point, vec2 start, vec2 end, float width) {
+  return roundCapDistanceField(point, start, end, width);
+}
+
+float bevelJoinField(vec2 point, vec2 start, vec2 end, float width, float joinAngle) {
+  vec2 startToPoint = point - start;
+  vec2 tangent = normalize(end - start);
+  float c = cos(joinAngle * 0.5);
+  float s = sin(joinAngle * 0.5);
+  float direction = -sign(sin(joinAngle));
+  vec2 bisector = vec2(c * tangent.x - s * tangent.y, s * tangent.x + c * tangent.y);
+  float radius = width * 0.5 * s;
+  return dot(startToPoint, bisector * direction) - radius;
+}
+
+float miterJoinDistanceField(vec2 point, vec2 start, vec2 end, float width, float joinAngle) {
+  if (cos(joinAngle) > 0.985) { // avoid risking a division by zero
+    return bevelJoinField(point, start, end, width, joinAngle);
+  }
+  float miterLength = 1. / sin(joinAngle * 0.5);
+  float miterLimit = 10.;
+  if (miterLength > miterLimit) {
+    return bevelJoinField(point, start, end, width, joinAngle);
+  }
+  return -1000.;
+}
+
+float capDistanceField(vec2 point, vec2 start, vec2 end, float width, float capType) {
+   if (capType == 4.0) {
+    return buttCapDistanceField(point, start, end);
+  } else if (capType == 5.0) {
+    return squareCapDistanceField(point, start, end, width);
+  }
+  return roundCapDistanceField(point, start, end, width);
+}
+
+float joinDistanceField(vec2 point, vec2 start, vec2 end, float width, float joinAngle, float joinType) {
+  if (joinType == 6.0) {
+    return bevelJoinField(point, start, end, width, joinAngle);
+  } else if (joinType == 7.0) {
+    return miterJoinDistanceField(point, start, end, width, joinAngle);
+  }
+  return roundJoinDistanceField(point, start, end, width);
+}
+
+float computeSegmentPointDistance(vec2 point, vec2 start, vec2 end, float width, float joinAngle, float capType, float joinType) {
+  if (isCap(joinAngle)) {
+    return capDistanceField(point, start, end, width, capType);
+  }
+  return joinDistanceField(point, start, end, width, joinAngle, joinType);
+}
+
+void main(void) {
+  vec2 currentPoint = gl_FragCoord.xy / u_pixelRatio;
+  #ifdef GL_FRAGMENT_PRECISION_HIGH
+  vec2 worldPos = pxToWorld(currentPoint);
+  if (
+    abs(u_renderExtent[0] - u_renderExtent[2]) > 0.0 && (
+      worldPos[0] < u_renderExtent[0] ||
+      worldPos[1] < u_renderExtent[1] ||
+      worldPos[0] > u_renderExtent[2] ||
+      worldPos[1] > u_renderExtent[3]
+    )
+  ) {
+    discard;
+  }
+  #endif
+
+  float segmentLength = length(v_segmentEnd - v_segmentStart);
+  vec2 segmentTangent = (v_segmentEnd - v_segmentStart) / segmentLength;
+  vec2 segmentNormal = vec2(-segmentTangent.y, segmentTangent.x);
+  vec2 startToPoint = currentPoint - v_segmentStart;
+  float lengthToPoint = max(0., min(dot(segmentTangent, startToPoint), segmentLength));
+  float currentLengthPx = lengthToPoint + v_distanceOffsetPx; 
+  float currentRadiusPx = abs(dot(segmentNormal, startToPoint));
+  float currentRadiusRatio = dot(segmentNormal, startToPoint) * 2. / v_width;
+  currentLineMetric = mix(v_measureStart, v_measureEnd, lengthToPoint / segmentLength);
+
+  if ((!((u_resolution < 600.0) && operator_in_0(v_prop_class) && (v_prop_layer == 3.0))) || (texture2D(u_depthMask, gl_FragCoord.xy / u_pixelRatio / u_viewportSizePx).r * 50. > u_tileZoomLevel + 0.5)) { discard; }
+
+  vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
+  float capType = 0.0;
+  float joinType = 0.0;
+  float segmentStartDistance = computeSegmentPointDistance(currentPoint, v_segmentStart, v_segmentEnd, v_width, v_angleStart, capType, joinType);
+  float segmentEndDistance = computeSegmentPointDistance(currentPoint, v_segmentEnd, v_segmentStart, v_width, v_angleEnd, capType, joinType);
+  float distance = max(
+    segmentDistanceField(currentPoint, v_segmentStart, v_segmentEnd, v_width),
+    max(segmentStartDistance, segmentEndDistance)
+  );
+  distance = max(distance, -1000.);
+  color.a *= smoothstep(0.5, -0.5, distance);
+  gl_FragColor = color;
+  gl_FragColor.a *= u_globalAlpha;
+  gl_FragColor.rgb *= gl_FragColor.a;
+  if (u_hitDetection > 0) {
+    if (gl_FragColor.a < 0.1) { discard; };
+    gl_FragColor = v_hitColor;
+  }
+}
