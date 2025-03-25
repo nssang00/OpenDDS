26932c39564d7769535f88bce466600cd5df79e8
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-03-26 00:50:40 +0900

    Update webpack.worker.config.js

--- js/10.4.0/webpack.worker.config.js
+++ js/10.4.0/webpack.worker.config.js
@@ -1,11 +1,37 @@
-const webpack = require('webpack');
+// webpack.worker.config.js
+const path = require('path');
+
+class BlobWrapperPlugin {
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('BlobWrapperPlugin', (compilation) => {
+      const workerContent = require('fs').readFileSync(
+        path.resolve(__dirname, 'webgl.worker.js'),
+        'utf8'
+      );
+      const blobWrappedContent = `
+        export function create() {
+          const source = ${JSON.stringify(workerContent)};
+          return new Worker(typeof Blob === 'undefined'
+            ? 'data:application/javascript;base64,' + btoa(source)
+            : URL.createObjectURL(new Blob([source], {type: 'application/javascript'})));
+        }
+      `;
+      require('fs').writeFileSync(
+        path.resolve(__dirname, './node_modules/ol/worker/webgl.js'),
+        blobWrappedContent
+      );
+      console.log('worker-blob.js generated successfully!');
+    });
+  }
+}
 
 module.exports = {
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   entry: './node_modules/ol/worker/webgl.worker.js',
-  //devtool: 'eval-source-map',  
+  //devtool: 'source-map',
   output: {
     path: __dirname,
-    filename: 'webgl.worker.js'
+    filename: './webgl.worker.js'
   },
+  plugins: [new BlobWrapperPlugin()]
 };
