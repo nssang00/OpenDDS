86830c3c287f59f08619f4cc7bc65a00a5c8c4de
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-04-02 09:17:19 +0900

    Delete js/10.4.0/webgl.worker.js

--- js/10.4.0/webgl.worker.js
+++
@@ -1,7 +0,0 @@
-{
-  "scripts": {
-    "build:worker": "webpack --config webpack.worker.config.js",
-    "build:main": "webpack --config webpack.main.config.js",
-    "build": "npm run build:worker && npm run build:main"
-  }
-}
