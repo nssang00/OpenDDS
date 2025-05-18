2e0706ae978db80f917eae09fb8bb597e5e31395
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-05-19 08:44:03 +0900

    Update NativeWindowHost.cs

--- js/10.4.0/NativeWindowHost.cs
+++ js/10.4.0/NativeWindowHost.cs
@@ -42,3 +42,36 @@ internal static class NativeMethods
     [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
     public static extern bool DestroyWindow(IntPtr hWnd);
 }
+
+/////
+public class NativeWindowHost : HwndHost
+{
+    private IntPtr _hwnd;
+
+    protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+    {
+        _hwnd = CreateWindowEx(0, "static", "",
+            WS_CHILD | WS_VISIBLE,
+            0, 0, 300, 200,
+            hwndParent.Handle, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+
+        return new HandleRef(this, _hwnd);
+    }
+
+    protected override void DestroyWindowCore(HandleRef hwnd)
+    {
+        DestroyWindow(hwnd.Handle);
+    }
+
+    private const int WS_CHILD = 0x40000000;
+    private const int WS_VISIBLE = 0x10000000;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CreateWindowEx(
+        int exStyle, string className, string windowName, int style,
+        int x, int y, int width, int height,
+        IntPtr hwndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool DestroyWindow(IntPtr hWnd);
+}
