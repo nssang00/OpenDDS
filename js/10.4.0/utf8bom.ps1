ec0003c4415cb59f931c82b3010226300aed17a6
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-05-30 16:30:42 +0900

    Delete js/10.4.0/utf8bom.ps1

--- js/10.4.0/utf8bom.ps1
+++
@@ -1,6 +0,0 @@
-# 현재 폴더 및 하위 폴더의 .cpp, .h 파일을 UTF-8 with BOM으로 변환
-Get-ChildItem -Recurse -Include *.cpp, *.h | ForEach-Object {
-    $content = Get-Content $_.FullName -Raw
-    $utf8BOM = New-Object System.Text.UTF8Encoding $true
-    [System.IO.File]::WriteAllText($_.FullName, $content, $utf8BOM)
-}
