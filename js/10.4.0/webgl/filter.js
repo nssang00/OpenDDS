a6202829aa5a88fd49089679f3e6d7585702ba96
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-07-16 11:32:02 +0900

    Update filter.js

--- js/10.4.0/webgl/filter.js
+++ js/10.4.0/webgl/filter.js
@@ -1,13 +1,18 @@
 const seenUids = new Set();
+const filteredFeatures = [];
 
-const filtered = features.filter((feature) => {
-  if (styleShader.featureFilter && !styleShader.featureFilter(feature)) {
-    return false;
+for (const styleShader of this.styleShaders) {
+  const filtered = styleShader.featureFilter
+    ? features.filter(styleShader.featureFilter)
+    : features;
+
+  for (const feature of filtered) {
+    const uid = feature.ol_uid;
+    if (!seenUids.has(uid)) {
+      seenUids.add(uid);
+      filteredFeatures.push(feature);
+    }
   }
-  const uid = feature.ol_uid;
-  if (seenUids.has(uid)) {
-    return false;
-  }
-  seenUids.add(uid);
-  return true;
-});
+}
+
+
