bd807e53938c0999830b25820d18062afeebf924
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-07-16 17:03:01 +0900

    Update generatePolygonBuffers_.js

--- js/10.4.0/webgl/generatePolygonBuffers_.js
+++ js/10.4.0/webgl/generatePolygonBuffers_.js
@@ -1,3 +1,72 @@
+export function generateLineStringRenderInstructionsFromFeatures(
+  features,
+  renderInstructions,
+  customAttributes,
+  transform,
+) {
+  let verticesCount = 0;
+  let geometriesCount = 0;
+  
+  // 배열을 features 길이만큼 미리 생성
+  const geometryRenderEntries = new Array(features.length);
+  
+  for (let i = 0; i < features.length; i++) {
+    const feature = features[i];
+    const geometry = feature.getGeometry();
+    const flatCoordinates = geometry.getFlatCoordinates();
+    const ends = geometry.getEnds();
+    const stride = geometry.getStride();
+  
+    verticesCount += flatCoordinates.length / stride;
+    geometriesCount += ends.length;
+  
+    const pixelCoordinates = new Array(flatCoordinates.length);
+    transform2D(
+      flatCoordinates, 
+      0, 
+      flatCoordinates.length, 
+      stride, 
+      transform, 
+      pixelCoordinates,
+      stride
+    );
+    
+ geometryRenderEntries[i] = { feature, pixelCoordinates, ends };
+    
+  }
+}
+
+/////////
+export function generatePointRenderInstructionsFromFeatures(
+  features,
+  renderInstructions,
+  customAttributes,
+  transform,
+) {
+  const geometriesCount = features.length;
+  const geometryRenderEntries = new Array(geometriesCount);
+
+  for (let i = 0; i < geometriesCount; i++) {
+    const feature = features[i];
+    const geometry = feature.getGeometry();
+    const flatCoordinates = geometry.getFlatCoordinates();
+
+    // 변환
+    const pixelCoordinates = new Array(flatCoordinates.length);
+    pixelCoordinates[0] = transform[0] * flatCoordinates[0] + transform[2] * flatCoordinates[1] + transform[4];
+    pixelCoordinates[1] = transform[1] * flatCoordinates[0] + transform[3] * flatCoordinates[1] + transform[5];
+
+    geometryRenderEntries[i] = { feature, pixelCoordinates };
+  }
+
+  // ... 이후 renderInstructions, customAttributes 사용 로직
+}
+
+
+
+
+
+
 const floatsPerSegment =
   2 +                // p0(x, y)
   1 +                // m0
