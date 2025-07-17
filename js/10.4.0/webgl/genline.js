5dc59aca1953ee190610a4682de59fea581e81d9
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-07-17 19:22:07 +0900

    Update genline.js

--- js/10.4.0/webgl/genline.js
+++ js/10.4.0/webgl/genline.js
@@ -1,35 +1,7 @@
-const worldCache = [
-  { idx: -1, world: null },
-  { idx: -1, world: null },
-  { idx: -1, world: null },
-  { idx: -1, world: null }
-];
-
-function getWorld(idx) {
-  if (idx === null || idx < 0 || idx >= verticesCount) return null;
-
-  for (let i = 0; i < 4; ++i) {
-    if (worldCache[i].idx === idx) return worldCache[i].world;
-  }
-
-  const instrIdx = idxToInstr(idx);
-  const world = applyTransform(invertTransform, [
-    renderInstructions[instrIdx],
-    renderInstructions[instrIdx + 1]
-  ]);
-
-  worldCache.shift();
-  worldCache.push({ idx, world });
-  return world;
-}
-
 
 function generateLineStringBuffers_(renderInstructions, customAttributesSize, transform) {
   const customAttrsCount = customAttributesSize;
   const instructionsPerVertex = 3;
-  const floatsPerSegment = 2+1+2+1+2+1+1+customAttrsCount;
-  const invertTransform = createTransform();
-  makeInverseTransform(invertTransform, transform);
 
   let currentInstructionsIndex = 0;
   let totalSegments = 0;
@@ -38,142 +10,7 @@ function generateLineStringBuffers_(renderInstructions, customAttributesSize, tr
     const verticesCount = renderInstructions[currentInstructionsIndex++];
     totalSegments += (verticesCount - 1);
     currentInstructionsIndex += verticesCount * instructionsPerVertex;
-  }
-  const totalFloats = totalSegments * floatsPerSegment;
-  const instanceAttributes = new Float32Array(totalFloats);
-  let bufferPos = 0;
-
-  currentInstructionsIndex = 0;
-  while (currentInstructionsIndex < renderInstructions.length) {
-    const customAttributes = [];
-    for (let i = 0; i < customAttrsCount; ++i)
-      customAttributes[i] = renderInstructions[currentInstructionsIndex + i];
-    currentInstructionsIndex += customAttrsCount;
-    const verticesCount = renderInstructions[currentInstructionsIndex++];
-
-    const baseIdx = currentInstructionsIndex;
-    const idxToInstr = idx => baseIdx + idx * instructionsPerVertex;
-
-    const firstInstructionsIndex = currentInstructionsIndex;
-    const lastInstructionsIndex = currentInstructionsIndex + (verticesCount - 1) * instructionsPerVertex;
-    const isLoop =
-      renderInstructions[firstInstructionsIndex] === renderInstructions[lastInstructionsIndex] &&
-      renderInstructions[firstInstructionsIndex + 1] === renderInstructions[lastInstructionsIndex + 1];
-
-    let currentLength = 0;
-    let currentAngleTangentSum = 0;
-
-    let worldB = null, world0 = null, world1 = null, worldA = null;
-    let cachedIdxB = -1, cachedIdx0 = -1, cachedIdx1 = -1, cachedIdxA = -1;
-
-    function getWorld(idx) {
-      if (idx === null || idx < 0 || idx >= verticesCount) return null;
-      
-      if (idx === cachedIdxB) return worldB;
-      if (idx === cachedIdx0) return world0;
-      if (idx === cachedIdx1) return world1;
-      if (idx === cachedIdxA) return worldA;
-      
-      const instrIdx = idxToInstr(idx);
-      const world = applyTransform(invertTransform, [
-        renderInstructions[instrIdx],
-        renderInstructions[instrIdx + 1]
-      ]);
-      
-      if (idx === cachedIdx0 + 1) {
-        worldB = world0;
-        cachedIdxB = cachedIdx0;
-        world0 = world1;
-        cachedIdx0 = cachedIdx1;
-        world1 = world;
-        cachedIdx1 = idx;
-      } else {
-        worldA = world;
-        cachedIdxA = idx;
-      }
-      
-      return world;
-    }
-
-    for (let i = 0; i < verticesCount - 1; ++i) {
-      const idx0 = i;
-      const idx1 = i + 1;
-      const idxB = (i > 0) ? i - 1 : (isLoop ? verticesCount - 2 : null);
-      const idxA = (i < verticesCount - 2) ? i + 2 : (isLoop ? 1 : null);
-
-      const p0idx = idxToInstr(idx0);
-      const p1idx = idxToInstr(idx1);
-      const p0 = [renderInstructions[p0idx], renderInstructions[p0idx + 1], renderInstructions[p0idx + 2]];
-      const p1 = [renderInstructions[p1idx], renderInstructions[p1idx + 1], renderInstructions[p1idx + 2]];
-      
-      const p0world = getWorld(idx0);
-      const p1world = getWorld(idx1);
-      const pBworld = getWorld(idxB);
-      const pAworld = getWorld(idxA);
-
-      function angleBetween(p0, pA, pB) {
-        const ax = pA[0] - p0[0], ay = pA[1] - p0[1];
-        const bx = pB[0] - p0[0], by = pB[1] - p0[1];
-        if ((ax * ax + ay * ay) < 1e-12 || (bx * bx + by * by) < 1e-12) return 0;
-        const angle = Math.atan2(ax * by - ay * bx, ax * bx + ay * by);
-        return angle < 0 ? angle + 2 * Math.PI : angle;
-      }
-
-      let angle0 = -1, angle1 = -1;
-      let newAngleTangentSum = currentAngleTangentSum;
-      if (pBworld) {
-        angle0 = angleBetween(p0world, p1world, pBworld);
-        if (Math.cos(angle0) <= 0.985)
-          newAngleTangentSum += Math.tan((angle0 - Math.PI) / 2);
-      }
-      if (pAworld) {
-        angle1 = angleBetween(p1world, p0world, pAworld);
-        if (Math.cos(angle1) <= 0.985)
-          newAngleTangentSum += Math.tan((Math.PI - angle1) / 2);
-      }
-
-      instanceAttributes[bufferPos++] = p0[0];
-      instanceAttributes[bufferPos++] = p0[1];
-      instanceAttributes[bufferPos++] = p0[2];
-      instanceAttributes[bufferPos++] = p1[0];
-      instanceAttributes[bufferPos++] = p1[1];
-      instanceAttributes[bufferPos++] = p1[2];
-      instanceAttributes[bufferPos++] = angle0;
-      instanceAttributes[bufferPos++] = angle1;
-      instanceAttributes[bufferPos++] = currentLength;
-      instanceAttributes[bufferPos++] = currentAngleTangentSum;
-      for (let j = 0; j < customAttrsCount; ++j)
-        instanceAttributes[bufferPos++] = customAttributes[j];
-
-      currentLength += Math.sqrt(
-        (p1world[0] - p0world[0]) * (p1world[0] - p0world[0]) +
-        (p1world[1] - p0world[1]) * (p1world[1] - p0world[1])
-      );
-      currentAngleTangentSum = newAngleTangentSum;
-    }
-    currentInstructionsIndex += verticesCount * instructionsPerVertex;
-  }
-
-  return {
-    indicesBuffer: new Uint32Array([0, 1, 3, 1, 2, 3]),
-    vertexAttributesBuffer: new Float32Array([-1, -1, 1, -1, 1, 1, -1, 1]),
-    instanceAttributesBuffer: instanceAttributes,
-  };
-}
-
-///////////
-function generateLineStringBuffers_(renderInstructions, customAttributesSize, transform) {
-  const customAttrsCount = customAttributesSize;
-  const instructionsPerVertex = 3;
-  
-  let currentInstructionsIndex = 0;
-  let totalSegments = 0;
-  while (currentInstructionsIndex < renderInstructions.length) {
-    currentInstructionsIndex += customAttrsCount;
-    const verticesCount = renderInstructions[currentInstructionsIndex++];
-    totalSegments += (verticesCount - 1);
-    currentInstructionsIndex += verticesCount * instructionsPerVertex;
-  }
+  }  
 
   const floatsPerSegment =
     2 +                // p0(x, y)
@@ -186,10 +23,10 @@ function generateLineStringBuffers_(renderInstructions, customAttributesSize, tr
     customAttrsCount;  // customAttrs
   const totalFloats = totalSegments * floatsPerSegment;
   const instanceAttributes = new Float32Array(totalFloats);
-  let bufferPos = 0;
+  let instanceOffset = 0;
 
   const invertTransform = createTransform();
-  makeInverseTransform(invertTransform, transform);  
+  makeInverseTransform(invertTransform, transform);
 
   currentInstructionsIndex = 0;
   while (currentInstructionsIndex < renderInstructions.length) {
@@ -208,30 +45,46 @@ function generateLineStringBuffers_(renderInstructions, customAttributesSize, tr
     let currentLength = 0;
     let currentAngleTangentSum = 0;
 
-    const worldCache = new Array(verticesCount);
+    const worldCoordCache = [];
 
-    function getWorld(idx) {
-      if (idx === null || idx < 0 || idx >= verticesCount) return null;
-      if (!worldCache[idx]) {
-        const instrIdx = currentInstructionsIndex + idx * instructionsPerVertex;
-        worldCache[idx] = applyTransform(invertTransform, [
-          renderInstructions[instrIdx],
-          renderInstructions[instrIdx + 1]
-        ]);
-      }
-      return worldCache[idx];
+    function getWorldCoord(index, offset) {
+      if (offset === null || offset < 0 || offset >= verticesCount) return null;
+
+      if (worldCoordCache[index]?.offset === offset) 
+        return worldCoordCache[index].world;
+
+      const segmentStartIndex = currentInstructionsIndex + offset * instructionsPerVertex;
+      const world = applyTransform(invertTransform, [
+        renderInstructions[segmentStartIndex],
+        renderInstructions[segmentStartIndex + 1]
+      ]);
+      worldCoordCache[index] = { offset, world };
+      return world;
     }
 
     for (let i = 0; i < verticesCount - 1; ++i) {
       let beforeIndex = null;
-      if (i > 0) beforeIndex = currentInstructionsIndex + (i - 1) * instructionsPerVertex;
-      else if (isLoop) beforeIndex = currentInstructionsIndex + (verticesCount - 2) * instructionsPerVertex;
+      if (i > 0) {
+        beforeIndex =
+          currentInstructionsIndex + (i - 1) * instructionsPerVertex;
+      } else if (isLoop) {
+        beforeIndex = lastInstructionsIndex - instructionsPerVertex;
+      }
       let afterIndex = null;
-      if (i < verticesCount - 2) afterIndex = currentInstructionsIndex + (i + 2) * instructionsPerVertex;
-      else if (isLoop) afterIndex = currentInstructionsIndex + instructionsPerVertex;
+      if (i < verticesCount - 2) {
+        afterIndex =
+          currentInstructionsIndex + (i + 2) * instructionsPerVertex;
+      } else if (isLoop) {
+        afterIndex = firstInstructionsIndex + instructionsPerVertex;
+      }
 
       const segmentStartIndex = currentInstructionsIndex + i * instructionsPerVertex;
       const segmentEndIndex   = currentInstructionsIndex + (i + 1) * instructionsPerVertex;
+      const p0 = [renderInstructions[segmentStartIndex], renderInstructions[segmentStartIndex + 1]];
+      const p1 = [renderInstructions[segmentEndIndex],   renderInstructions[segmentEndIndex + 1]];
+      const m0 = renderInstructions[segmentStartIndex + 2];
+      const m1 = renderInstructions[segmentEndIndex + 2];
+
       const idx0 = (segmentStartIndex - currentInstructionsIndex) / instructionsPerVertex;
       const idx1 = (segmentEndIndex   - currentInstructionsIndex) / instructionsPerVertex;
       let idxB = null, idxA = null;
@@ -240,21 +93,11 @@ function generateLineStringBuffers_(renderInstructions, customAttributesSize, tr
       if (afterIndex !== null)
         idxA = (afterIndex - currentInstructionsIndex) / instructionsPerVertex;
 
-      const p0 = [
-        renderInstructions[segmentStartIndex],
-        renderInstructions[segmentStartIndex + 1],
-        renderInstructions[segmentStartIndex + 2]
-      ];
-      const p1 = [
-        renderInstructions[segmentEndIndex],
-        renderInstructions[segmentEndIndex + 1],
-        renderInstructions[segmentEndIndex + 2]
-      ];
-
-      const p0world = getWorld(idx0);
-      const p1world = getWorld(idx1);
-      const pBworld = getWorld(idxB);
-      const pAworld = getWorld(idxA);
+      const pBworld = getWorldCoord(0, idxB);
+      const p0world = getWorldCoord(1, idx0);
+      const p1world = getWorldCoord(2, idx1);
+      const pAworld = getWorldCoord(3, idxA);
+      worldCoordCache.shift();
 
       function angleBetween(p0, pA, pB) {
         const ax = pA[0] - p0[0], ay = pA[1] - p0[1];
@@ -266,156 +109,29 @@ function generateLineStringBuffers_(renderInstructions, customAttributesSize, tr
 
       let angle0 = -1, angle1 = -1;
       let newAngleTangentSum = currentAngleTangentSum;
-      if (pBworld) {
+      if (beforeIndex !== null) {
         angle0 = angleBetween(p0world, p1world, pBworld);
-        if (Math.cos(angle0) <= 0.985)
+        if (Math.cos(angle0) <= 0.985)//LINESTRING_ANGLE_COSINE_CUTOFF
           newAngleTangentSum += Math.tan((angle0 - Math.PI) / 2);
       }
-      if (pAworld) {
+      if (afterIndex !== null) {
         angle1 = angleBetween(p1world, p0world, pAworld);
-        if (Math.cos(angle1) <= 0.985)
+        if (Math.cos(angle1) <= 0.985)//LINESTRING_ANGLE_COSINE_CUTOFF
           newAngleTangentSum += Math.tan((Math.PI - angle1) / 2);
       }
 
-      // 기록
-      instanceAttributes[bufferPos++] = p0[0];
-      instanceAttributes[bufferPos++] = p0[1];
-      instanceAttributes[bufferPos++] = p0[2];
-      instanceAttributes[bufferPos++] = p1[0];
-      instanceAttributes[bufferPos++] = p1[1];
-      instanceAttributes[bufferPos++] = p1[2];
-      instanceAttributes[bufferPos++] = angle0;
-      instanceAttributes[bufferPos++] = angle1;
-      instanceAttributes[bufferPos++] = currentLength;
-      instanceAttributes[bufferPos++] = currentAngleTangentSum;
+      instanceAttributes[instanceOffset++] = p0[0];
+      instanceAttributes[instanceOffset++] = p0[1];
+      instanceAttributes[instanceOffset++] = m0;
+      instanceAttributes[instanceOffset++] = p1[0];
+      instanceAttributes[instanceOffset++] = p1[1];
+      instanceAttributes[instanceOffset++] = m1;
+      instanceAttributes[instanceOffset++] = angle0;
+      instanceAttributes[instanceOffset++] = angle1;
+      instanceAttributes[instanceOffset++] = currentLength;
+      instanceAttributes[instanceOffset++] = currentAngleTangentSum;
       for (let j = 0; j < customAttrsCount; ++j)
-        instanceAttributes[bufferPos++] = customAttributes[j];
-
-      currentLength += Math.sqrt(
-        (p1world[0] - p0world[0]) * (p1world[0] - p0world[0]) +
-        (p1world[1] - p0world[1]) * (p1world[1] - p0world[1])
-      );
-      currentAngleTangentSum = newAngleTangentSum;
-    }
-    currentInstructionsIndex += verticesCount * instructionsPerVertex;
-  }
-
-  return {
-    indicesBuffer: new Uint32Array([0, 1, 3, 1, 2, 3]),
-    vertexAttributesBuffer: new Float32Array([-1, -1, 1, -1, 1, 1, -1, 1]),
-    instanceAttributesBuffer: instanceAttributes,
-  };
-}
-
-
-
-///////////
-function generateLineStringBuffers_(renderInstructions, customAttributesSize, transform) {
-  const customAttrsCount = customAttributesSize;
-  const instructionsPerVertex = 3;
-  const floatsPerSegment = 2+1+2+1+2+1+1+customAttrsCount;
-  const invertTransform = createTransform();
-  makeInverseTransform(invertTransform, transform);
-
-  let currentInstructionsIndex = 0;
-  let totalSegments = 0;
-  while (currentInstructionsIndex < renderInstructions.length) {
-    currentInstructionsIndex += customAttrsCount;
-    const verticesCount = renderInstructions[currentInstructionsIndex++];
-    totalSegments += (verticesCount - 1);
-    currentInstructionsIndex += verticesCount * instructionsPerVertex;
-  }
-  const totalFloats = totalSegments * floatsPerSegment;
-  const instanceAttributes = new Float32Array(totalFloats);
-  let bufferPos = 0;
-
-  currentInstructionsIndex = 0;
-  while (currentInstructionsIndex < renderInstructions.length) {
-    // customAttributes
-    const customAttributes = [];
-    for (let i = 0; i < customAttrsCount; ++i)
-      customAttributes[i] = renderInstructions[currentInstructionsIndex + i];
-    currentInstructionsIndex += customAttrsCount;
-    const verticesCount = renderInstructions[currentInstructionsIndex++];
-
-    const baseIdx = currentInstructionsIndex;
-    const idxToInstr = idx => baseIdx + idx * instructionsPerVertex;
-
-    // loop 여부
-    const firstInstructionsIndex = currentInstructionsIndex;
-    const lastInstructionsIndex = currentInstructionsIndex + (verticesCount - 1) * instructionsPerVertex;
-    const isLoop =
-      renderInstructions[firstInstructionsIndex] === renderInstructions[lastInstructionsIndex] &&
-      renderInstructions[firstInstructionsIndex + 1] === renderInstructions[lastInstructionsIndex + 1];
-
-    let currentLength = 0;
-    let currentAngleTangentSum = 0;
-
-    // ===== 최소 수정 캐시 =====
-    const worldCache = new Array(verticesCount);
-
-    function getWorld(idx) {
-      if (idx === null || idx < 0 || idx >= verticesCount) return null;
-      if (!worldCache[idx]) {
-        const instrIdx = idxToInstr(idx);
-        worldCache[idx] = applyTransform(invertTransform, [
-          renderInstructions[instrIdx],
-          renderInstructions[instrIdx + 1]
-        ]);
-      }
-      return worldCache[idx];
-    }
-
-    for (let i = 0; i < verticesCount - 1; ++i) {
-      // idx 계산 (기존 코드 거의 그대로 유지)
-      const idx0 = i;
-      const idx1 = i + 1;
-      const idxB = (i > 0) ? i - 1 : (isLoop ? verticesCount - 2 : null);
-      const idxA = (i < verticesCount - 2) ? i + 2 : (isLoop ? 1 : null);
-
-      const p0idx = idxToInstr(idx0);
-      const p1idx = idxToInstr(idx1);
-      const p0 = [renderInstructions[p0idx], renderInstructions[p0idx + 1], renderInstructions[p0idx + 2]];
-      const p1 = [renderInstructions[p1idx], renderInstructions[p1idx + 1], renderInstructions[p1idx + 2]];
-      const p0world = getWorld(idx0);
-      const p1world = getWorld(idx1);
-      const pBworld = getWorld(idxB);
-      const pAworld = getWorld(idxA);
-
-      function angleBetween(p0, pA, pB) {
-        const ax = pA[0] - p0[0], ay = pA[1] - p0[1];
-        const bx = pB[0] - p0[0], by = pB[1] - p0[1];
-        if ((ax * ax + ay * ay) < 1e-12 || (bx * bx + by * by) < 1e-12) return 0;
-        const angle = Math.atan2(ax * by - ay * bx, ax * bx + ay * by);
-        return angle < 0 ? angle + 2 * Math.PI : angle;
-      }
-
-      let angle0 = -1, angle1 = -1;
-      let newAngleTangentSum = currentAngleTangentSum;
-      if (pBworld) {
-        angle0 = angleBetween(p0world, p1world, pBworld);
-        if (Math.cos(angle0) <= 0.985)
-          newAngleTangentSum += Math.tan((angle0 - Math.PI) / 2);
-      }
-      if (pAworld) {
-        angle1 = angleBetween(p1world, p0world, pAworld);
-        if (Math.cos(angle1) <= 0.985)
-          newAngleTangentSum += Math.tan((Math.PI - angle1) / 2);
-      }
-
-      // 기록
-      instanceAttributes[bufferPos++] = p0[0];
-      instanceAttributes[bufferPos++] = p0[1];
-      instanceAttributes[bufferPos++] = p0[2];
-      instanceAttributes[bufferPos++] = p1[0];
-      instanceAttributes[bufferPos++] = p1[1];
-      instanceAttributes[bufferPos++] = p1[2];
-      instanceAttributes[bufferPos++] = angle0;
-      instanceAttributes[bufferPos++] = angle1;
-      instanceAttributes[bufferPos++] = currentLength;
-      instanceAttributes[bufferPos++] = currentAngleTangentSum;
-      for (let j = 0; j < customAttrsCount; ++j)
-        instanceAttributes[bufferPos++] = customAttributes[j];
+        instanceAttributes[instanceOffset++] = customAttributes[j];
 
       currentLength += Math.sqrt(
         (p1world[0] - p0world[0]) * (p1world[0] - p0world[0]) +
