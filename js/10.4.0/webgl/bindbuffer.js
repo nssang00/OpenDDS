d81ae61792a164aa21fdb3ea606711f617df472a
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-07-22 19:19:04 +0900

    Update bindbuffer.js

--- js/10.4.0/webgl/bindbuffer.js
+++ js/10.4.0/webgl/bindbuffer.js
@@ -1,3 +1,45 @@
+    return [
+      this.helper_.flushBufferData2(ELEMENT_ARRAY_BUFFER, buffers.indicesBuffer, DYNAMIC_DRAW),
+      this.helper_.flushBufferData2(ARRAY_BUFFER, buffers.vertexAttributesBuffer, DYNAMIC_DRAW),
+      this.helper_.flushBufferData2(ARRAY_BUFFER, buffers.instanceAttributesBuffer, DYNAMIC_DRAW),
+    ];   
+
+ bindBuffer2(buffer) {
+    const gl = this.gl_;
+
+    let bufferCache = null;
+    if(this.webglBufferCache_.has(buffer)) {
+      bufferCache = this.webglBufferCache_.get(buffer);
+    }
+    gl.bindBuffer(bufferCache.target, buffer);
+  }
+
+  getSize2(buffer) {
+    const gl = this.gl_;
+
+    let bufferCache = null;
+    if(this.webglBufferCache_.has(buffer)) {
+      bufferCache = this.webglBufferCache_.get(buffer);
+    }
+    return bufferCache.size;
+  }
+      
+  flushBufferData2(target, buffer, usage) {
+    const gl = this.gl_;
+    const webGlBuffer = gl.createBuffer();
+    const bufferCache = { target: target, size: buffer?.length ?? 0};
+    this.webglBufferCache_.set(webGlBuffer, bufferCache);
+
+    this.bindBuffer2(webGlBuffer);
+    gl.bufferData(target, buffer, usage);
+    return webGlBuffer;
+  } 
+  deleteBuffer2(buf) {
+    //const bufferKey = getUid(buf);
+    // Note: gl.deleteBuffer is not called here since we let WebGL garbage collect it automatically
+    delete this.webglBufferCache_.delete(buf);
+  }
+
 class WebGLBufferCache {
   constructor() {
     this.cache = new WeakMap();
