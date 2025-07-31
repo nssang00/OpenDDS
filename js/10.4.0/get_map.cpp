ec48e12e68db6a39f51ee8ad86c90e7e5dd4aeca
nssang00 <34025004+nssang00@users.noreply.github.com>
2025-07-31 18:35:23 +0900

    Update get_map.cpp

--- js/10.4.0/get_map.cpp
+++ js/10.4.0/get_map.cpp
@@ -1,9 +1,11 @@
-#include <windows.h>
-#include <iphlpapi.h>
-#include <ws2tcpip.h>
+#include <winsock2.h>     
+#include <ws2tcpip.h>      // getnameinfo
+#include <windows.h>       
+#include <iphlpapi.h>      
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
 #pragma comment(lib, "iphlpapi.lib")
 #pragma comment(lib, "ws2_32.lib")
 
