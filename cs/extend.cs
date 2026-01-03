07a454136e2788d0ce4eecf9237073150e33ed24
nssang00 <34025004+nssang00@users.noreply.github.com>
2026-01-03 15:26:48 +0900

    Update extend.cs

--- cs/extend.cs
+++ cs/extend.cs
@@ -19,3 +19,28 @@ const char* GetGraphicTypeName(SGISGraphicBase2D* obj) {
     return "SGISGraphicBase2D";
 }
 %}
+
+%inline %{
+const char* GetObjectTypeName(SGISGraphicBase2D* obj) {
+    if (!obj) return nullptr;
+
+    return typeid(*obj).name();
+
+}
+%}
+///
+%module YourModule
+
+// std::vector 래핑
+%include "std_vector.i"
+
+namespace std {
+    %template(SGISGraphicObjectList) vector<SGISGraphicBase2D*>;
+}
+
+// typemap으로 getitem 커스터마이징 (선택사항)
+%typemap(csout, excode=SWIGEXCODE) SGISGraphicBase2D* {
+    System.IntPtr cPtr = $imcall;$excode
+    return (cPtr == System.IntPtr.Zero) ? null : 
+           GraphicObjectFactory.CreateFromPtr(cPtr, false);
+}
