37270686f876a7e48e975b66024762a5230953e4
nssang00 <34025004+nssang00@users.noreply.github.com>
2026-01-28 19:13:01 +0900

    Update VariantToJson.h

--- customallocator/VariantToJson.h
+++ customallocator/VariantToJson.h
@@ -3,69 +3,45 @@
 #include <vector>
 #include <map>
 #include <any>
-#include <sstream>
-#include <iomanip>
 
 typedef std::map<std::string, std::any> VariantDict;
 typedef std::vector<std::any> VariantList;
 typedef std::any Variant;
 
-// 문자열 내 특수문자 이스케이프 처리 (JSON 규격 준수)
-std::string EscapeJsonString(const std::string& s) {
-    std::ostringstream oss;
-    oss << '"';
-    for (char c : s) {
-        switch (c) {
-            case '"':  oss << "\\\""; break;
-            case '\\': oss << "\\\\"; break;
-            case '\b': oss << "\\b";  break;
-            case '\f': oss << "\\f";  break;
-            case '\n': oss << "\\n";  break;
-            case '\r': oss << "\\r";  break;
-            case '\t': oss << "\\t";  break;
-            default:   oss << c;      break;
-        }
-    }
-    oss << '"';
-    return oss.str();
-}
-
 std::string VariantToJsonString(const Variant& v) {
-    // 숫자형
-    if (v.type() == typeid(int)) return std::to_string(std::any_cast<int>(v));
-    if (v.type() == typeid(long long)) return std::to_string(std::any_cast<long long>(v));
-    if (v.type() == typeid(double)) return std::to_string(std::any_cast<double>(v));
-    if (v.type() == typeid(float)) return std::to_string(std::any_cast<float>(v));
-    if (v.type() == typeid(bool)) return std::any_cast<bool>(v) ? "true" : "false";
-    
-    // 문자열
-    if (v.type() == typeid(std::string)) {
-        return EscapeJsonString(std::any_cast<std::string>(v));
-    }
-    if (v.type() == typeid(const char*)) {
-        return EscapeJsonString(std::any_cast<const char*>(v));
-    }
-
-    // List
-    if (v.type() == typeid(VariantList)) {
+    if (v.type() == typeid(int)) {
+        return std::to_string(std::any_cast<int>(v));
+    } else if (v.type() == typeid(long long)) {
+        return std::to_string(std::any_cast<long long>(v));
+    } else if (v.type() == typeid(double)) {
+        return std::to_string(std::any_cast<double>(v));
+    } else if (v.type() == typeid(float)) {
+        return std::to_string(std::any_cast<float>(v));
+    } else if (v.type() == typeid(bool)) {
+        return std::any_cast<bool>(v) ? "true" : "false";
+    } 
+    else if (v.type() == typeid(std::string)) {
+        return "\"" + std::any_cast<std::string>(v) + "\"";
+    } 
+    else if (v.type() == typeid(VariantList)) {
         const auto& list = std::any_cast<const VariantList&>(v);
         std::string res = "[";
         for (size_t i = 0; i < list.size(); ++i) {
             res += VariantToJsonString(list[i]);
-            if (i + 1 < list.size()) res += ",";  // 약간 더 명확
+            if (i + 1 < list.size()) 
+                res += ",";
         }
         res += "]";
         return res;
-    }
-
-    // Dict
-    if (v.type() == typeid(VariantDict)) {
+    } 
+    else if (v.type() == typeid(VariantDict)) {
         const auto& dict = std::any_cast<const VariantDict&>(v);
         std::string res = "{";
         size_t count = 0;
-        for (const auto& [key, value] : dict) {  // structured binding 사용
-            res += EscapeJsonString(key) + ":" + VariantToJsonString(value);
-            if (++count < dict.size()) res += ",";
+        for (const auto& [key, value] : dict) {
+            res += "\"" + key + "\":" + VariantToJsonString(value);
+            if (++count < dict.size()) 
+                res += ",";
         }
         res += "}";
         return res;
