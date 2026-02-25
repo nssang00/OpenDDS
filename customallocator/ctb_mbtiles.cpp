c3edb3875ce45c326c98bb556b79a1b0a4d791bd
nssang00 <34025004+nssang00@users.noreply.github.com>
2026-02-26 08:34:06 +0900

    Update copyright notice in ctb_mbtiles.cpp

--- customallocator/ctb_mbtiles.cpp
+++ customallocator/ctb_mbtiles.cpp
@@ -1,4 +1,5 @@
-ata <geodata@soton.ac.uk>
+/*******************************************************************************
+ * Copyright 2014 GeoData <geodata@soton.ac.uk>
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
