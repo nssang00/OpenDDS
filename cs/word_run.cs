a8c14716d0f7024fe9d1fc32d3c2f55caeaba27e
nssang00 <34025004+nssang00@users.noreply.github.com>
2026-03-13 19:54:29 +0900

    Create word_run.cs

---
+++ cs/word_run.cs
@@ -0,0 +1,40 @@
+<w:tr>
+  <w:tc>
+    <w:p>
+      <w:r>
+        <w:t>#fore</w:t>   ← Word가 임의로 쪼갬
+      </w:r>
+      <w:r>
+        <w:rPr><w:rFonts w:ascii="Calibri"/></w:rPr>
+        <w:t>ach($att</w:t>
+      </w:r>
+      <w:r>
+        <w:t xml:space="preserve">r in $element.Owned</w:t>
+      </w:r>
+      <w:r>
+        <w:t>Attribute)</w:t>
+      </w:r>
+    </w:p>
+  </w:tc>
+</w:tr>
+
+//////
+<w:tr>
+  <w:tc>
+    <w:p>
+      <w:r>
+        <w:t>#fore</w:t>   ← Word가 임의로 쪼갬
+      </w:r>
+      <w:r>
+        <w:rPr><w:rFonts w:ascii="Calibri"/></w:rPr>
+        <w:t>ach($att</w:t>
+      </w:r>
+      <w:r>
+        <w:t xml:space="preserve">r in $element.Owned</w:t>
+      </w:r>
+      <w:r>
+        <w:t>Attribute)</w:t>
+      </w:r>
+    </w:p>
+  </w:tc>
+</w:tr>
